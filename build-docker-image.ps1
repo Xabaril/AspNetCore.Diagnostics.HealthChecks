dc869c7d1f2d13d4f186248793d5b952d0711b9e
unaizorrilla <unai@plainconcepts.com>
2018-10-22 21:26:03 +0200

    working on docker image for healthchecks ui

--- build-docker-image.ps1
+++ build-docker-image.ps1
@@ -2,17 +2,7 @@ Param(
     [parameter(Mandatory=$false)][bool]$PublishToDockerHub=$false
 )
 
-# Taken from psake https://github.com/psake/psake
 
-<#
-.SYNOPSIS
-  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
-  to see if an error occcured. If an error is detected then an exception is thrown.
-  This function allows you to run command-line programs without having to
-  explicitly check the $lastexitcode variable.
-.EXAMPLE
-  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
-#>
 function Exec
 {
     [CmdletBinding()]
@@ -28,6 +18,7 @@ function Exec
 
 #Select the UI version from dependencies.props and use it as image version
 
+
 $version = select-xml -Path .\build\dependencies.props -XPath "/Project/PropertyGroup[contains(@Label,'Health Checks Package Versions')]/HealthCheckUI"
 
 $tag = $version.node.InnerXML
@@ -36,8 +27,8 @@ $tag = $version.node.InnerXML
 
 echo "building docker image with tag: $tag"
 
-exec { & docker build . -f .\docker-images\HealthChecksUI-Image\Dockerfile -t xabarilcoding/healthchecksui:$tag }
-exec { & docker tag xabarilcoding/beatpulseui:$tag xabarilcoding/healthchecksui:latest }
+exec { & docker build . -f .\build\docker-images\HealthChecks.UI.Image\Dockerfile -t xabarilcoding/healthchecksui:$tag }
+exec { & docker tag xabarilcoding/healthchecksui:$tag xabarilcoding/healthchecksui:latest }
 
 echo "Created docker image healthchecksui:$tag. You can execute this image using docker run"
 echo "Sample: docker run --name ui -p 5000:80 -e 'HealthChecks-UI:HealthChecks:0:Name=httpBasic' -e 'HealthChecks-UI:HealthChecks:0:Uri=http://www.google.es' -d healthchecksui:dev"
---
+++ build/docker-images/HealthChecks.UI.Image/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthChecks.UI.Image
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddHealthChecksUI()
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            
+
+            app.UseHealthChecksUI()
+                .UseMvc();
+        }
+    }
+}

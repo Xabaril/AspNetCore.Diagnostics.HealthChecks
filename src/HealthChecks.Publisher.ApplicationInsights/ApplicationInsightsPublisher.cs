6718bae12936b80dc1edfa22c9039a1b6ee3b707
Daniel Edwards <daniel.edwards@esendex.com>
2018-12-13 08:33:48 +0000

    Made TelemtryClient static for ApplicationInsights publisher

--- src/HealthChecks.Publisher.ApplicationInsights/ApplicationInsightsPublisher.cs
+++ src/HealthChecks.Publisher.ApplicationInsights/ApplicationInsightsPublisher.cs
@@ -16,24 +16,22 @@ namespace HealthChecks.Publisher.ApplicationInsights
         const string METRIC_STATUS_NAME = "AspNetCoreHealthCheckStatus";
         const string METRIC_DURATION_NAME = "AspNetCoreHealthCheckDuration";
 
-        private readonly string _instrumentationKey;
+        private static TelemetryClient _client;
 
         public ApplicationInsightsPublisher(string instrumentationKey = default)
         {
-            _instrumentationKey = instrumentationKey;
+            //override instrumentation key or use default instrumentation 
+            //key active on the project.
+
+            var configuration = string.IsNullOrWhiteSpace(instrumentationKey)
+                ? TelemetryConfiguration.Active
+                : new TelemetryConfiguration(instrumentationKey);
+            _client = new TelemetryClient(configuration);
         }
 
         public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
         {
-            //override instrumentation key or use default instrumentation 
-            //key active on the project.
-            var configuration = String.IsNullOrWhiteSpace(_instrumentationKey)
-                ? TelemetryConfiguration.Active
-                : new TelemetryConfiguration(_instrumentationKey);
-
-            var client = new TelemetryClient(configuration);
-
-            client.TrackEvent(EVENT_NAME,
+            _client.TrackEvent(EVENT_NAME,
                 properties: new Dictionary<string, string>()
                 {
                     {nameof(Environment.MachineName),Environment.MachineName},

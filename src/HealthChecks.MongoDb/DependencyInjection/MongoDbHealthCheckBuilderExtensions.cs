719c5bcacc12de331771bb7061c8a0beb385da9c
Unai Zorrilla Castro <unai@plainconcepts.com>
2018-12-20 00:57:00 +0100

    added support on mongodb healthcheck to list collection on specified database

--- src/HealthChecks.MongoDb/DependencyInjection/MongoDbHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.MongoDb/DependencyInjection/MongoDbHealthCheckBuilderExtensions.cs
@@ -9,7 +9,7 @@ namespace Microsoft.Extensions.DependencyInjection
         const string NAME = "mongodb";
 
         /// <summary>
-        /// Add a health check for Kafka cluster.
+        /// Add a health check for MongoDb database that list all databases on the system.
         /// </summary>
         /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
         /// <param name="mongodbConnectionString">The MongoDb connection string to be used.</param>
@@ -25,7 +25,29 @@ namespace Microsoft.Extensions.DependencyInjection
             return builder.Add(new HealthCheckRegistration(
                 name ?? NAME,
                 sp => new MongoDbHealthCheck(mongodbConnectionString),
-                failureStatus ,
+                failureStatus,
+                tags));
+        }
+
+        /// <summary>
+        /// Add a health check for MongoDb database that list all collections from specified database on <paramref name="mongoDatabaseName"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="mongodbConnectionString">The MongoDb connection string to be used.</param>
+        /// <param name="mongoDatabaseName">The Database name to check.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'mongodb' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns></param>
+        public static IHealthChecksBuilder AddMongoDb(this IHealthChecksBuilder builder, string mongodbConnectionString, string mongoDatabaseName, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default)
+        {
+            return builder.Add(new HealthCheckRegistration(
+                name ?? NAME,
+                sp => new MongoDbHealthCheck(mongodbConnectionString, mongoDatabaseName),
+                failureStatus,
                 tags));
         }
     }

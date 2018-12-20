0fd894890661ea653a1712384b6901415106fbbe
Unai Zorrilla Castro <unai@plainconcepts.com>
2018-12-20 21:42:41 +0100

    fixing minor issues

--- src/HealthChecks.Publisher.Seq/DependencyInjection/SeqHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Publisher.Seq/DependencyInjection/SeqHealthCheckBuilderExtensions.cs
@@ -9,14 +9,26 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         const string NAME = "seq";
 
+        /// <summary>
+        ///     Add a health check publisher for Seq.
+        /// </summary>
+        /// <remarks>
+        ///     For each <see cref="HealthReport" /> published a new metric value indicating the health check status ( 2 Healthy, 1
+        ///     Degraded, 0 Unhealthy)  and the total time the health check took to execute on seconds./>
+        /// </remarks>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder" />.</param>
+        /// <param name="setup">The Sql configuration options.</param>
+        /// <param name="name"> The registration name. This is also the associated http client name if you use AddHttpClient </param>
+        /// <returns>The <see cref="IHealthChecksBuilder" />.</returns>
         public static IHealthChecksBuilder AddSeqPublisher(this IHealthChecksBuilder builder, Action<SeqOptions> setup, string name = default)
         {
+            builder.Services.AddHttpClient();
+
             var options = new SeqOptions();
-            setup(options);
+            setup?.Invoke(options);
 
             var registrationName = name ?? NAME;
-            builder.Services.AddHttpClient(registrationName, client => client.BaseAddress = new Uri(options.Endpoint));
-
+            
             builder.Services.AddSingleton<IHealthCheckPublisher>(sp =>
             {
                 return new SeqPublisher(() => sp.GetRequiredService<IHttpClientFactory>().CreateClient(registrationName), options);
--- src/HealthChecks.Publisher.Seq/SeqPublisher.cs
+++ src/HealthChecks.Publisher.Seq/SeqPublisher.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
@@ -6,15 +9,12 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Newtonsoft.Json;
 
 namespace HealthChecks.Publisher.Seq
 {
     public class SeqPublisher : IHealthCheckPublisher
     {
-        private const string EVENT_NAME = "AspNetCoreHealthCheck";
+        private const string EVENT_NAME = "AspNetCoreHealthCheckResult";
         private const string METRIC_STATUS_NAME = "AspNetCoreHealthCheckStatus";
         private const string METRIC_DURATION_NAME = "AspNetCoreHealthCheckDuration";
 
@@ -26,7 +26,6 @@ namespace HealthChecks.Publisher.Seq
             _options = options;
             _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
         }
-
         public async Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
         {
             var level = "Information";
@@ -46,7 +45,7 @@ namespace HealthChecks.Publisher.Seq
                 {
                     new RawEvent
                     {
-                        Timestamp = DateTimeOffset.Now,
+                        Timestamp = DateTimeOffset.UtcNow,
                         MessageTemplate = $"{EVENT_NAME}",
                         Level = level,
                         Properties = new Dictionary<string, object>()
@@ -59,36 +58,35 @@ namespace HealthChecks.Publisher.Seq
                     }
                 }
             };
+
             await PushMetrics(JsonConvert.SerializeObject(events));
         }
-
         private async Task PushMetrics(string json)
         {
             try
             {
                 var httpClient = _httpClientFactory();
-                var response = await httpClient.PostAsync($"/api/events/raw?apiKey={_options.ApiKey}", new StringContent(json, Encoding.UTF8, "application/json"));
-                response.EnsureSuccessStatusCode();
+
+                var pushMessage = new HttpRequestMessage(HttpMethod.Post, $"{_options.Endpoint}/api/events/raw?apiKey={_options.ApiKey}");
+                pushMessage.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                (await httpClient.SendAsync(pushMessage))
+                    .EnsureSuccessStatusCode();
             }
-            catch (Exception ex) when (!(ex is OperationCanceledException))
+            catch (Exception ex)
             {
-                Trace.WriteLine($"Error in PushMetrics: {ex.Message}");
+                Trace.WriteLine($"Exception is throwed publishing metrics to Seq with message: {ex.Message}");
             }
         }
-
         private class RawEvents
         {
             public RawEvent[] Events { get; set; }
         }
-
         private class RawEvent
         {
             public DateTimeOffset Timestamp { get; set; }
-
             public string Level { get; set; }
-
             public string MessageTemplate { get; set; }
-
             public Dictionary<string, object> Properties { get; set; }
         }
     }

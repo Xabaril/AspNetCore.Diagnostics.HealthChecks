b2b81356be40c7eab28d634d18880b0277199896
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2019-01-22 11:48:03 +0100

    Azure Table Storage + Test

---
+++ test/UnitTests/DependencyInjection/AzureStorage/AzureTableStorageUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using HealthChecks.AzureStorage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using Xunit;
+
+namespace UnitTests.DependencyInjection.AzureStorage
+{
+    public class azuretabletorage_registration_should
+    {
+        [Fact]
+        public void not_thrown_when_connectionstring_is_invalid()
+        {
+            var connectionString = "bla";
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                    .AddAzureTableStorage(connectionString);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("azuretable");
+            check.GetType().Should().Be(typeof(AzureTableStorageHealthCheck));
+        }
+    }
+}

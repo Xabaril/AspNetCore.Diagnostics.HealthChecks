acba2c0301bc028b2fdcb308c40b8800fc045a2c
Peter Winkler <pwr@pi-applications.dk>
2019-01-27 17:25:17 +0100

    Added hangfire server check

--- src/HealthChecks.Hangfire/DependencyInjection/HangfireHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Hangfire/DependencyInjection/HangfireHealthCheckBuilderExtensions.cs
@@ -7,14 +7,15 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class HangfireHealthCheckBuilderExtensions
     {
-        const string NAME = "hangfire";
+        const string NAME_FAILED = "hangfire-failed";
+        const string NAME_SERVERS = "hangfire-server";
 
         /// <summary>
         /// Add a health check for Hangfire
         /// </summary>
         /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
         /// <param name="setup">The action to configure the Hangfire parameters.</param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'hangfire' will be used for the name.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'hangfire-failed' will be used for the name.</param>
         /// <param name="failureStatus">
         /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
         /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
@@ -27,8 +28,29 @@ namespace Microsoft.Extensions.DependencyInjection
             setup?.Invoke(hangfireOptions);
 
             return builder.Add(new HealthCheckRegistration(
-               name ?? NAME,
+               name ?? NAME_FAILED,
                sp => new HangfireHealthCheck(hangfireOptions),
+               failureStatus,
+               tags));
+        }
+
+        /// <summary>
+        /// Add a health check for Hangfire servers
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="minimumServers">Minimum of Hangfire servers</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'hangfire-servers' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns></param>
+        public static IHealthChecksBuilder AddHangfireServers(this IHealthChecksBuilder builder, int minimumServers = 1, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default)
+        {
+            return builder.Add(new HealthCheckRegistration(
+               name ?? NAME_SERVERS,
+               sp => new HangfireServerHealthCheck(minimumServers),
                failureStatus,
                tags));
         }
---
+++ src/HealthChecks.Hangfire/HangfireServerHealthCheck.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecks.Hangfire
+{
+    public class HangfireServerHealthCheck
+        : IHealthCheck
+    {
+        private readonly int _minimumServers;
+
+        public HangfireServerHealthCheck(int minimumServers)
+        {
+            _minimumServers = minimumServers;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var hangfireMonitoringApi = global::Hangfire.JobStorage.Current.GetMonitoringApi();
+
+                var serversCount = hangfireMonitoringApi.Servers().Count;
+                if (serversCount < _minimumServers)
+                {
+                    return Task.FromResult(new HealthCheckResult(context.Registration.FailureStatus, description: $"{serversCount} server registered. Expected minimum {_minimumServers}"));
+                }
+                    
+                return Task.FromResult(HealthCheckResult.Healthy());
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new HealthCheckResult(context.Registration.FailureStatus, exception: ex));
+            }
+        }
+    }
+}

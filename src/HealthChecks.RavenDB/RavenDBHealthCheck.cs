522979ee58f4322c4e35feeb5a4ca6fb66509c4a
Miňo Martiniak <mino.martiniak@gmail.com>
2019-02-01 09:30:03 +0100

    Remove unnecessary comment

--- src/HealthChecks.RavenDB/RavenDBHealthCheck.cs
+++ src/HealthChecks.RavenDB/RavenDBHealthCheck.cs
@@ -29,19 +29,6 @@ namespace HealthChecks.RavenDB
             _specifiedDatabase = databaseName;
         }
 
-        /// <summary>
-        /// Runs the health check, returning the status of the RavenDB.
-        /// </summary>
-        /// <param name="context">
-        /// A context object associated with the current execution.
-        /// </param>
-        /// <param name="cancellationToken">
-        /// A <see cref="T:System.Threading.CancellationToken" /> that can be used to cancel the health check.
-        /// </param>
-        /// <returns>
-        /// A <see cref="T:System.Threading.Tasks.Task`1" /> that completes when the health check has finished,
-        /// yielding the status of the component being checked.
-        /// </returns>
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             try

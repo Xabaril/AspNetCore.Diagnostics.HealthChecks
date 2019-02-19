e1bf6c29fea1073f06d7cdcd2bec9ad0573ba315
Alexandru RUS <arus@isoftwaresystems.com>
2019-02-19 16:29:39 +0200

    added the ability to setup settings

--- src/HealthChecks.UI/ServiceCollectionExtensions.cs
+++ src/HealthChecks.UI/ServiceCollectionExtensions.cs
@@ -6,7 +6,6 @@ using HealthChecks.UI.Core.Discovery.K8S.Extensions;
 using HealthChecks.UI.Core.HostedService;
 using HealthChecks.UI.Core.Notifications;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
@@ -19,19 +18,20 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddHealthChecksUI(this IServiceCollection services, string databaseName = "healthchecksdb")
+        public static IServiceCollection AddHealthChecksUI(this IServiceCollection services, string databaseName = "healthchecksdb", Action<Settings> setupSettings = null)
         {
             var configuration = services.BuildServiceProvider()
                 .GetService<IConfiguration>();
 
             var healthCheckSettings = new Settings();
             var kubernetesDiscoverySettings = new KubernetesDiscoverySettings();
-            configuration.Bind(Keys.HEALTHCHECKSUI_SECTION_SETTING_KEY, healthCheckSettings);
+
+            BindHealthChecksUISettings(setupSettings, configuration, healthCheckSettings);
             configuration.Bind(Keys.HEALTHCHECKSUI_KUBERNETES_DISCOVERY_SETTING_KEY, kubernetesDiscoverySettings);
 
             services
                 .AddOptions()
-                .Configure<Settings>(settings => configuration.Bind(Keys.HEALTHCHECKSUI_SECTION_SETTING_KEY, settings))
+                .Configure<Settings>(settings => BindHealthChecksUISettings(setupSettings, configuration, settings))
                 .AddSingleton<IHostedService, HealthCheckCollectorHostedService>()
                 .AddScoped<IHealthCheckFailureNotifier, WebHookFailureNotifier>()
                 .AddScoped<IHealthCheckReportCollector, HealthCheckReportCollector>()
@@ -46,7 +46,7 @@ namespace Microsoft.Extensions.DependencyInjection
                     var path = Path.Combine(contentRoot, databaseName);
                     connectionString = $"Data Source={path}";
                 }
-                else 
+                else
                 {
                     connectionString = Environment.ExpandEnvironmentVariables(connectionString);
                 }
@@ -69,6 +69,13 @@ namespace Microsoft.Extensions.DependencyInjection
 
             return services;
         }
+
+        private static void BindHealthChecksUISettings(Action<Settings> passedInSettings, IConfiguration configuration, Settings healthCheckSettings)
+        {
+            passedInSettings?.Invoke(healthCheckSettings);
+            configuration.Bind(Keys.HEALTHCHECKSUI_SECTION_SETTING_KEY, healthCheckSettings);
+        }
+
         static async Task CreateDatabase(IServiceProvider serviceProvider)
         {
             var scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();

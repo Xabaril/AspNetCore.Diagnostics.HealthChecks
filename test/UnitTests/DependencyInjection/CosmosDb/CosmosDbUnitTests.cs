80bb1c67141930e7b3663d3f22063790e4ce7d1c
Ahmed Magdy <ahmedmagdi@outlook.com>
2019-04-04 00:01:47 +0200

    Add Cosmosdb unit test

---
+++ test/UnitTests/DependencyInjection/CosmosDb/CosmosDbUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using HealthChecks.CosmosDb;
+using Xunit;
+
+namespace UnitTests.HealthChecks.DependencyInjection.CosmosDb
+{
+    public class documentdb_registration_should
+    {
+        [Fact]
+        public void add_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCosmosDb("myconnectionstring");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("cosmosdb");
+            check.GetType().Should().Be(typeof(CosmosDbHealthCheck));
+        }
+        [Fact]
+        public void add_named_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCosmosDb("myconnectionstring", name: "my-cosmosdb-group");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("my-cosmosdb-group");
+            check.GetType().Should().Be(typeof(CosmosDbHealthCheck));
+        }
+    }
+}

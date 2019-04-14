d57d77eee6e38ce5bd83bff054b05b9cac8e405e
Odonno <bottiau.david@laposte.net>
2019-04-14 23:38:27 +0200

    tests: add dependency injection tests

---
+++ test/UnitTests/DependencyInjection/CloudFirestore/CloudFirestoreUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using HealthChecks.Gcp.CloudFirestore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using Xunit;
+
+namespace UnitTests.HealthChecks.DependencyInjection.CloudFirestore
+{
+    public class cloud_firestore_registration_should
+    {
+        [Fact]
+        public void add_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCloudFirestore(setup => setup.RequiredCollections = new string[] { });
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("cloud firestore");
+            check.GetType().Should().Be(typeof(CloudFirestoreHealthCheck));
+        }
+        [Fact]
+        public void add_named_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCloudFirestore(setup => setup.RequiredCollections = new string[] { }, name: "my-cloud-firestore-group");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("my-cloud-firestore-group");
+            check.GetType().Should().Be(typeof(CloudFirestoreHealthCheck));
+        }
+    }
+}

811bbfb3abdfc390b4bec1eb1215b421a61631bb
Kevin-Schumm <kevin.schumm@limeade.com>
2019-04-29 11:45:00 -0700

    Fix spelling/grammer issues in HangfireHealthCheck error message.

--- src/HealthChecks.Hangfire/HangfireHealthCheck.cs
+++ src/HealthChecks.Hangfire/HangfireHealthCheck.cs
@@ -27,7 +27,7 @@ namespace HealthChecks.Hangfire
                     var failedJobsCount = hangfireMonitoringApi.FailedCount();
                     if (failedJobsCount >= _hangfireOptions.MaximumJobsFailed)
                     {
-                        errorList.Add($"Hangfire have #{failedJobsCount} failed jobs and the maximun available is {_hangfireOptions.MaximumJobsFailed}.");
+                        errorList.Add($"Hangfire has #{failedJobsCount} failed jobs and the maximum available is {_hangfireOptions.MaximumJobsFailed}.");
                     }
                 }
 

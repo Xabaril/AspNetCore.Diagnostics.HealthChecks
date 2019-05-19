2d867c907c2e3bf6b1b2aae26d43a9141bf6fb37
Carlos Landeras <carlos.landeras@gmail.com>
2019-05-19 13:54:51 +0200

    #159 Handle multi segments paths for UI resources

--- src/HealthChecks.UI/Core/Extensions/UIResourceExtensions.cs
+++ src/HealthChecks.UI/Core/Extensions/UIResourceExtensions.cs
@@ -10,17 +10,21 @@ namespace HealthChecks.UI.Core
         public static UIResource GetMainUI(this IEnumerable<UIResource> resources, Options options)
         {
             var resource = resources
-                .Where(r => r.ContentType == ContentType.HTML && r.FileName == Keys.HEALTHCHECKSUI_MAIN_UI_RESOURCE)
-                .FirstOrDefault();
+                .FirstOrDefault(r => r.ContentType == ContentType.HTML && r.FileName == Keys.HEALTHCHECKSUI_MAIN_UI_RESOURCE);
 
             resource.Content = resource.Content
-                .Replace(Keys.HEALTHCHECKSUI_MAIN_UI_API_TARGET, options.ApiPath.AsRelativeResource());
+                .Replace(Keys.HEALTHCHECKSUI_MAIN_UI_API_TARGET, options.ApiPath);
 
             resource.Content = resource.Content
                 .Replace(Keys.HEALTHCHECKSUI_WEBHOOKS_API_TARGET, options.WebhookPath);
 
+            
+            var isMultisegmentUI = options.UIPath.Count(c => c == '/') > 1;
+
+            var resourcePath = isMultisegmentUI ? options.ResourcesPath : options.ResourcesPath.AsRelativeResource();
+            
             resource.Content = resource.Content
-                .Replace(Keys.HEALTHCHECKSUI_RESOURCES_TARGET,options.ResourcesPath.AsRelativeResource());
+                .Replace(Keys.HEALTHCHECKSUI_RESOURCES_TARGET, resourcePath);
 
             return resource;
         }

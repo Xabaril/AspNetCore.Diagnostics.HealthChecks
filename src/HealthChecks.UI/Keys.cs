74ff6498fb49d3e8c22a20a2e552f9b080f1014e
Carlos Landeras <carlos.landeras@gmail.com>
2019-06-30 08:49:48 +0200

    Update Kubernetes discovery service to new section name

--- src/HealthChecks.UI/Keys.cs
+++ src/HealthChecks.UI/Keys.cs
@@ -8,7 +8,7 @@ namespace HealthChecks.UI
         internal const string HEALTHCHECKSUI_SECTION_SETTING_KEY = "HealthChecksUI";
         internal const string HEALTHCHECKS_DEFAULT_PATH = "hc";
         internal const string HEALTHCHECKS_DEFAULT_DISCOVERY_LABEL = "HealthChecks";
-        internal const string HEALTHCHECKSUI_KUBERNETES_DISCOVERY_SETTING_KEY = "HealthChecks-UI:KubernetesDiscoveryService";        
+        internal const string HEALTHCHECKSUI_KUBERNETES_DISCOVERY_SETTING_KEY = "HealthChecksUI:KubernetesDiscoveryService";        
         internal const string HEALTHCHECKSUI_MAIN_UI_RESOURCE = "index.html";
         internal const string HEALTHCHECKSUI_MAIN_UI_API_TARGET = "#apiPath#";
         internal const string HEALTHCHECKSUI_WEBHOOKS_API_TARGET = "#webhookPath#";

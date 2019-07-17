a1c2df493e75b9dc64ff3f12085e71c0a45ed441
Gerard Godone-Maresca <ggodonemaresca+github@gmail.com>
2019-07-16 22:03:26 -0400

    Implement in-cluster config

--- src/HealthChecks.UI/Core/Discovery/K8S/Extensions/KubernetesHttpClientExtensions.cs
+++ src/HealthChecks.UI/Core/Discovery/K8S/Extensions/KubernetesHttpClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -13,8 +14,35 @@ namespace HealthChecks.UI.Core.Discovery.K8S.Extensions
         {
             var options = serviceProvider.GetRequiredService<KubernetesDiscoverySettings>();
             
-            var validHttpEndpoint = Uri.TryCreate(options.ClusterHost, UriKind.Absolute, out var host)
-                && (host.Scheme == Uri.UriSchemeHttp || host.Scheme == Uri.UriSchemeHttps);
+            string token;
+            string hostString;
+
+            if(options.InCluster)
+            {
+                var clusterHost = Environment.GetEnvironmentVariable("KUBERNETES_SERVICE_HOST");
+                var clusterPort = Environment.GetEnvironmentVariable("KUBERNETES_SERVICE_PORT");
+
+                string fullClusterHost;
+                // Support IPv6 address hosts
+                if(clusterHost.Contains(":"))
+                {
+                    hostString = $"https://[{clusterHost}]:{clusterPort}";
+                }
+                else
+                {
+                    hostString = $"https://{clusterHost}:{clusterPort}";
+                }
+
+                token = File.ReadAllText("/var/run/secrets/kubernetes.io/serviceaccount/token").Trim();
+            }
+            else
+            {
+                token = options.Token;
+                hostString = options.ClusterHost;
+            }
+            
+            var validHttpEndpoint = Uri.TryCreate(hostString, UriKind.Absolute, out var host)
+                    && (host.Scheme == Uri.UriSchemeHttp || host.Scheme == Uri.UriSchemeHttps);
 
             if (!validHttpEndpoint)
             {

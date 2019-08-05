7308c3893a02d13d027fa3b9f287e9ffd264e6ca
Eva Crespo <ecrespo@plainconcepts.com>
2019-08-05 08:15:13 +0200

    Restyled navbar and created styles variables.

---
+++ src/HealthChecks.UI/client/components/AsideMenu.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+const MenuIcon = require('../../assets/svg/menu.svg');
+interface AsideMenuProps {
+  onClick: () => void;
+  children: any;
+  isOpen: boolean;
+}
+
+const AsideMenu = (props: AsideMenuProps) => {
+  return (
+    <aside
+      className={`hc-aside ${props.isOpen ? 'hc-aside--open' : ''}`}>
+      <button
+        className="hc-aside__open-btn"
+        onClick={(event) => {event.preventDefault(); return(props.onClick())}}>
+        <img src={MenuIcon} />
+      </button>
+      {/* <div className="hc-aside__logo" /> */}
+      <nav className="hc-aside-menu">{props.children}</nav>
+    </aside>
+  );
+};
+
+export { AsideMenu };

eaa79853a2ed0f36813b68a97b8f162f68847a31
Anton Hryshchanka <Anton.Hryshchanka_EX@kdrp.com>
2019-11-01 13:04:08 +0300

    Add Azure IoT Hub

---
+++ src/HealthChecks.AzureIoTHub/AzureIoTHubtHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace HealthChecks.AzureIoTHub
+{
+    public class AzureIoTHubHealthCheck : IHealthCheck
+    {
+        private readonly string _connectionString;
+        private readonly string _healthQuery;
+        public AzureIoTHubHealthCheck(string connectionString, string healthQuery)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            _healthQuery = healthQuery ?? throw new ArgumentNullException(nameof(healthQuery));
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using (var client = RegistryManager.CreateFromConnectionString(_connectionString))
+                {
+                    var query = client.CreateQuery(_healthQuery, 1);
+                    var _ = await query.GetNextAsJsonAsync();
+                }
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+            }
+        }
+    }
+}
---
+++ src/HealthChecks.AzureIoTHub/DependencyInjection/AzureIoTHubHealthChecksBuilderExtensions.cs
@@ -0,0 +1,83 @@
+﻿using HealthChecks.AzureIoTHub;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AzureIoTHubHealthChecksBuilderExtensions
+    {
+        private const string HEALTH_QUERY = "SELECT deviceId FROM devices";
+        const string IOTHUB_NAME = "azureiothub";
+
+        /// <summary>
+        /// Add a health check for Azure IoT Hub. Default behaviour is using Managed Service Identity, to use Client Secrets call UseClientSecrets in setup action
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="connectionString">Azure IoT Hub connection string to use.</param>
+        /// <param name="healthQuery">The query to be executed.Optional. If <c>null</c> select devices with some deviceId is used.</param>    
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'azureiothub' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
+        public static IHealthChecksBuilder AddAzureIoTHub(this IHealthChecksBuilder builder,
+            string connectionString,
+            string healthQuery = default,
+            string name = default,
+            HealthStatus? failureStatus = default,
+            IEnumerable<string> tags = default,
+            TimeSpan? timeout = default)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            return builder.Add(new HealthCheckRegistration(
+               name ?? IOTHUB_NAME,
+               sp => new AzureIoTHubHealthCheck(connectionString, healthQuery ?? HEALTH_QUERY),
+               failureStatus,
+               tags,
+               timeout));
+        }
+
+        /// <summary>
+        /// Add a health check for Azure IoT Hub. Default behaviour is using Managed Service Identity, to use Client Secrets call UseClientSecrets in setup action
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="connectionStringFactory">A factory to build the Azure IoT Hub connection string to use.</param>
+        /// <param name="healthQuery">The query to be executed.Optional. If <c>null</c> select devices with some deviceId is used.</param>    
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'azureiothub' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
+        public static IHealthChecksBuilder AddAzureIoTHub(this IHealthChecksBuilder builder,
+            Func<IServiceProvider, string> connectionStringFactory,
+            string healthQuery = default,
+            string name = default,
+            HealthStatus? failureStatus = default,
+            IEnumerable<string> tags = default,
+            TimeSpan? timeout = default)
+        {
+            if (connectionStringFactory == null)
+            {
+                throw new ArgumentNullException(nameof(connectionStringFactory));
+            }
+            
+            return builder.Add(new HealthCheckRegistration(
+               name ?? IOTHUB_NAME,
+               sp => new AzureIoTHubHealthCheck(connectionStringFactory(sp), healthQuery ?? HEALTH_QUERY),
+               failureStatus,
+               tags,
+               timeout));
+        }
+    }
+}

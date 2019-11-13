f0d3d2e43a62a912dc324d94591a24e7ed5e1550
Narayana Rengaswamy <narayana.rengaswamy@optym.com>
2019-11-13 17:01:47 +0530

    Handle cases where 1. queue does not exist when health check is run 2. queue gets deleted after health check is up, and gets created with a different session requirement

--- src/HealthChecks.AzureServiceBus/AzureServiceBusQueueHealthCheck.cs
+++ src/HealthChecks.AzureServiceBus/AzureServiceBusQueueHealthCheck.cs
@@ -15,10 +15,15 @@ namespace HealthChecks.AzureServiceBus
         private const string TEST_MESSAGE = "HealthCheckTest";
         private const string TEST_SESSIONID = "TestSessionId";
         private static readonly ConcurrentDictionary<string, QueueClient> _queueClientConnections = new ConcurrentDictionary<string, QueueClient>();
-        private static bool _requiresSession;
 
         private readonly string _connectionString;
         private readonly string _queueName;
+
+        /// <summary>
+        /// Null value for this variable specifies not being able to connect to queue yet to know session requirement.
+        /// This could happen if queue is not yet created or gets deleted, after health check starts.
+        /// </summary>
+        private bool? _requiresSession;
         private readonly Action<Message> _configuringMessage;
 
         public AzureServiceBusQueueHealthCheck(string connectionString, string queueName, Action<Message> configuringMessage = null)
@@ -42,26 +47,30 @@ namespace HealthChecks.AzureServiceBus
                 var connectionKey = $"{_connectionString}_{_queueName}";
                 if (!_queueClientConnections.TryGetValue(connectionKey, out var queueClient))
                 {
-                    queueClient = new QueueClient(_connectionString, _queueName,ReceiveMode.PeekLock, RetryPolicy.NoRetry);
+                    queueClient = new QueueClient(_connectionString, _queueName, ReceiveMode.PeekLock, RetryPolicy.NoRetry);
 
                     if (!_queueClientConnections.TryAdd(connectionKey, queueClient))
                     {
                         return new HealthCheckResult(context.Registration.FailureStatus, description: "New QueueClient connection can't be added into dictionary.");
                     }
+                }
 
+                // If the queue is not yet available, GetQueueAsync will throw and _requiresSession will not have any value
+                if (!_requiresSession.HasValue)
+                {
                     var managementClient = new ManagementClient(_connectionString);
                     var queueDescription = await managementClient.GetQueueAsync(_queueName);
                     _requiresSession = queueDescription.RequiresSession;
                 }
 
                 var message = new Message(Encoding.UTF8.GetBytes(TEST_MESSAGE));
-                if (_requiresSession)
+                if (_requiresSession.Value)
                 {
                     message.SessionId = TEST_SESSIONID;
                 }
                 _configuringMessage?.Invoke(message);
-                
-                var scheduledMessageId = await queueClient.ScheduleMessageAsync(message, 
+
+                var scheduledMessageId = await queueClient.ScheduleMessageAsync(message,
                     new DateTimeOffset(DateTime.UtcNow).AddHours(2));
 
                 await queueClient.CancelScheduledMessageAsync(scheduledMessageId);
@@ -69,6 +78,9 @@ namespace HealthChecks.AzureServiceBus
             }
             catch (Exception ex)
             {
+                // Reset _requiresSession to handle cases of queue not available once the health check probe is up
+                // If queue becomes available at a later time, we should again figure out whether sessions are needed.
+                _requiresSession = null;
                 return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
             }
         }

e9edad0ca20e24c0e5ac1c92e2c984107d41d678
Antoxa <antoxa.zimm@gmail.com>
2019-11-16 13:59:49 +0300

    Code review fixes, azure iot hub
    Change device id generation for registry write check
    Change approach for verify registry write check
    Remove default values in options

--- src/HealthChecks.AzureIoTHub/AzureIoTHubOptions.cs
+++ src/HealthChecks.AzureIoTHub/AzureIoTHubOptions.cs
@@ -5,9 +5,9 @@ namespace HealthChecks.AzureIoTHub
     public class AzureIoTHubOptions
     {
         internal string ConnectionString { get; }
-        internal bool RegistryReadCheck { get; private set; } = false;
-        internal bool RegistryWriteCheck { get; private set; } = false;
-        internal bool ServiceConnectionCheck { get; private set; } = false;
+        internal bool RegistryReadCheck { get; private set; }
+        internal bool RegistryWriteCheck { get; private set; }
+        internal bool ServiceConnectionCheck { get; private set; }
         internal string RegistryReadQuery { get; private set; }
         internal Func<string> RegistryWriteDeviceIdFactory { get; private set; }
         internal ServiceConnectionTransport ServiceConnectionTransport { get; set; }
@@ -30,7 +30,7 @@ namespace HealthChecks.AzureIoTHub
         public AzureIoTHubOptions AddRegistryWriteCheck(Func<string> deviceIdFactory = null)
         {
             RegistryWriteCheck = true;
-            RegistryWriteDeviceIdFactory = deviceIdFactory ?? (() => $"health-check-registry-write-{Guid.NewGuid()}");
+            RegistryWriteDeviceIdFactory = deviceIdFactory ?? (() => "health-check-registry-write-device-id");
             return this;
         }
         public AzureIoTHubOptions AddServiceConnectionCheck(ServiceConnectionTransport transport = ServiceConnectionTransport.Amqp)
--- src/HealthChecks.AzureIoTHub/AzureIoTHubtHealthCheck.cs
+++ src/HealthChecks.AzureIoTHub/AzureIoTHubtHealthCheck.cs
@@ -19,30 +19,15 @@ namespace HealthChecks.AzureIoTHub
             {
                 if (_options.RegistryWriteCheck)
                 {
-                    using (var client = RegistryManager.CreateFromConnectionString(_options.ConnectionString))
-                    {
-                        var deviceId = _options.RegistryWriteDeviceIdFactory();
-
-                        await client.AddDeviceAsync(new Device(deviceId), cancellationToken);
-                        await client.RemoveDeviceAsync(deviceId, cancellationToken);
-                    }
+                    await ExecuteRegistryWriteCheck(cancellationToken);
                 }
                 else if (_options.RegistryReadCheck)
                 {
-                    using (var client = RegistryManager.CreateFromConnectionString(_options.ConnectionString))
-                    {
-                        var query = client.CreateQuery(_options.RegistryReadQuery, 1);
-                        var _ = await query.GetNextAsJsonAsync();
-                    }
+                    await ExecuteRegistryReadCheck();
                 }
                 if (_options.ServiceConnectionCheck)
                 {
-                    var transportType = MapToTransportType(_options.ServiceConnectionTransport);
-                    using (var client = ServiceClient.CreateFromConnectionString(_options.ConnectionString, transportType))
-                    {
-                        var result = await client.GetServiceStatisticsAsync(cancellationToken);
-                        var _ = result.ConnectedDeviceCount;
-                    }
+                    await ExecuteServiceConnectionCheck(cancellationToken);
                 }
 
                 return HealthCheckResult.Healthy();
@@ -50,6 +35,55 @@ namespace HealthChecks.AzureIoTHub
             catch (Exception ex)
             {
                 return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+            }
+        }
+
+        private async Task ExecuteServiceConnectionCheck(CancellationToken cancellationToken)
+        {
+            var transportType = MapToTransportType(_options.ServiceConnectionTransport);
+            using (var client = ServiceClient.CreateFromConnectionString(_options.ConnectionString, transportType))
+            {
+                var result = await client.GetServiceStatisticsAsync(cancellationToken);
+                var _ = result.ConnectedDeviceCount;
+            }
+        }
+
+        private async Task ExecuteRegistryReadCheck()
+        {
+            using (var client = RegistryManager.CreateFromConnectionString(_options.ConnectionString))
+            {
+                var query = client.CreateQuery(_options.RegistryReadQuery, 1);
+                var _ = await query.GetNextAsJsonAsync();
+            }
+        }
+
+        /// <summary>
+        /// This method try create and remove device for registry write check.
+        /// </summary>
+        /// <remarks>
+        /// In default implementation of configuration <code>deviceId</code> equals <code>"health-check-registry-write-device-id"</code>.
+        /// If in previous health check device were not removed, try remove it.
+        /// If in previous health check device were added and removed, try create and remove it.
+        /// </remarks>
+        private async Task ExecuteRegistryWriteCheck(CancellationToken cancellationToken)
+        {
+            using (var client = RegistryManager.CreateFromConnectionString(_options.ConnectionString))
+            {
+                var deviceId = _options.RegistryWriteDeviceIdFactory();
+                var device = await client.GetDeviceAsync(deviceId, cancellationToken);
+
+                // in default implementation of configuration deviceId equals "health-check-registry-write-device-id"
+                // if in previous health check device were not removed -- try remove it
+                // if in previous health check device were added and removed -- try create and remove it
+                if (device != null)
+                {
+                    await client.RemoveDeviceAsync(deviceId, cancellationToken);
+                }
+                else
+                {
+                    await client.AddDeviceAsync(new Device(deviceId), cancellationToken);
+                    await client.RemoveDeviceAsync(deviceId, cancellationToken);
+                }
             }
         }
 

4f813092930e947e5ca7c8b00f6a3f4c40f56ce5
Karg <rkarg@blizzard.com>
2019-12-03 14:27:52 -0800

    Using valid Uri string for tests

--- test/UnitTests/DependencyInjection/RabbitMQ/RabbitMQUnitTests.cs
+++ test/UnitTests/DependencyInjection/RabbitMQ/RabbitMQUnitTests.cs
@@ -10,12 +10,15 @@ namespace UnitTests.HealthChecks.DependencyInjection.RabbitMQ
 {
     public class rabbitmq_registration_should
     {
+        private string _fakeConnectionString = "amqp://server";
+        private string _defaultCheckName = "rabbitmq";
+
         [Fact]
         public void add_health_check_when_properly_configured()
         {
             var services = new ServiceCollection();
             services.AddHealthChecks()
-                .AddRabbitMQ(rabbitMQConnectionString: "connectionstring");
+                .AddRabbitMQ(rabbitMQConnectionString: _fakeConnectionString);
 
             var serviceProvider = services.BuildServiceProvider();
             var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
@@ -23,7 +26,7 @@ namespace UnitTests.HealthChecks.DependencyInjection.RabbitMQ
             var registration = options.Value.Registrations.First();
             var check = registration.Factory(serviceProvider);
 
-            registration.Name.Should().Be("rabbitmq");
+            registration.Name.Should().Be(_defaultCheckName);
             check.GetType().Should().Be(typeof(RabbitMQHealthCheck));
         }
 
@@ -31,8 +34,10 @@ namespace UnitTests.HealthChecks.DependencyInjection.RabbitMQ
         public void add_named_health_check_when_properly_configured()
         {
             var services = new ServiceCollection();
+            var customCheckName = "my-"+ _defaultCheckName;
+
             services.AddHealthChecks()
-                .AddRabbitMQ("connectionstring", name: "my-rabbitmq");
+                .AddRabbitMQ(_fakeConnectionString, name: customCheckName);
 
             var serviceProvider = services.BuildServiceProvider();
             var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
@@ -40,7 +45,7 @@ namespace UnitTests.HealthChecks.DependencyInjection.RabbitMQ
             var registration = options.Value.Registrations.First();
             var check = registration.Factory(serviceProvider);
 
-            registration.Name.Should().Be("my-rabbitmq");
+            registration.Name.Should().Be(customCheckName);
             check.GetType().Should().Be(typeof(RabbitMQHealthCheck));
         }
     }

85c72d1bcab98ddf0b5e6c9c75d576cda104b3a9
MysticBoy <mysticboy@live.com>
2020-02-22 20:02:18 +0800

    add IHealthChecksDb

---
+++ src/HealthChecks.UI/Core/Data/IHealthChecksDb.cs
@@ -0,0 +1,19 @@
+﻿using HealthChecks.UI.Core.Data.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthChecks.UI.Core.Data
+{
+    public interface IHealthChecksDb
+    {
+        DbSet<HealthCheckConfiguration> Configurations { get; set; }
+
+        DbSet<HealthCheckExecution> Executions { get; set; }
+
+        DbSet<HealthCheckFailureNotification> Failures { get; set; }
+
+        DbSet<HealthCheckExecutionEntry> HealthCheckExecutionEntries { get; set; }
+        DbSet<HealthCheckExecutionHistory> HealthCheckExecutionHistories { get; set; }
+
+
+    }
+}

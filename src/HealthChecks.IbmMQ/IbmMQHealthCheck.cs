45fe6fccd9e5e40f174252847c2f6fa24d41e422
fglaeser <fglaeser@gmail.com>
2020-03-03 10:03:21 -0300

    add ibmmq healthchecks

---
+++ src/HealthChecks.IbmMQ/IbmMQHealthCheck.cs
@@ -0,0 +1,38 @@
+﻿using IBM.WMQ;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecks.IbmMQ
+{
+  public class IbmMQHealthCheck : IHealthCheck
+  {
+    private readonly Hashtable _connectionProperties;
+    private readonly string _queueManager;
+
+    public IbmMQHealthCheck(string queueManager, Hashtable connectionProperties)
+    {
+      _queueManager = queueManager;
+      _connectionProperties = connectionProperties;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+      try
+      {
+        using (var connection = new MQQueueManager(_queueManager, _connectionProperties))
+        {
+          return Task.FromResult(
+              HealthCheckResult.Healthy());
+        }
+      }
+      catch (Exception ex)
+      {
+        return Task.FromResult(
+            new HealthCheckResult(context.Registration.FailureStatus, exception: ex));
+      }
+    }
+  }
+}

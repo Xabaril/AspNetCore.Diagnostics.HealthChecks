a4da5f10408922469b9a62a5bac6d5a05e64a710
Dallas <dallasbeek@gmail.com>
2020-03-17 00:22:21 -0500

    Check if .well-known/openid-configuration is valid json response

--- src/HealthChecks.IdentityServer/IdentityServerHealthCheck.cs
+++ src/HealthChecks.IdentityServer/IdentityServerHealthCheck.cs
@@ -1,18 +1,20 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
 using System.Net.Http;
+using System.Runtime.Serialization.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 
-namespace HealthChecks.IdentityServer 
+namespace HealthChecks.IdentityServer
 {
-    public class IdentityServerHealthCheck 
-        : IHealthCheck 
+    public class IdentityServerHealthCheck
+        : IHealthCheck
     {
         const string IDENTITY_SERVER_DISCOVER_CONFIGURATION_SEGMENT = ".well-known/openid-configuration"; 
 
         private readonly Func<HttpClient> _httpClientFactory;
-        public IdentityServerHealthCheck(Func<HttpClient> httpClientFactory) 
+        public IdentityServerHealthCheck(Func<HttpClient> httpClientFactory)
         {
             _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
         }
@@ -21,14 +23,17 @@ namespace HealthChecks.IdentityServer
             try
             {
                 var httpClient = _httpClientFactory();
-                var response = await httpClient.GetAsync(IDENTITY_SERVER_DISCOVER_CONFIGURATION_SEGMENT, cancellationToken); 
+                var response = await httpClient.GetAsync(IDENTITY_SERVER_DISCOVER_CONFIGURATION_SEGMENT, cancellationToken);
 
                 if (!response.IsSuccessStatusCode)
                 {
                     return new HealthCheckResult(context.Registration.FailureStatus, description: $"Discover endpoint is not responding with 200 OK, the current status is {response.StatusCode} and the content { await response.Content.ReadAsStringAsync() }");
                 }
 
-                return HealthCheckResult.Healthy();       
+                // is it a valid json document?
+                JsonReaderWriterFactory.CreateJsonReader(await response.Content.ReadAsStreamAsync(), new XmlDictionaryReaderQuotas()).Read();
+
+                return HealthCheckResult.Healthy();
             }
             catch (Exception ex)
             {

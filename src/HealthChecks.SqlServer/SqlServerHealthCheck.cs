45ed6fc93097dc54d8d527f85bca7152bfc20390
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-04-08 18:50:03 +0200

    Upgrade sql server to use Microsoft.Data.SqlClient instead of System.Data.SqlClient

--- src/HealthChecks.SqlServer/SqlServerHealthCheck.cs
+++ src/HealthChecks.SqlServer/SqlServerHealthCheck.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
-using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 

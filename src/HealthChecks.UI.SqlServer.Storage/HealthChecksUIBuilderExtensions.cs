036a5975773e514d5f00065ae65e04afa05eb37c
Carlos Landeras <carlos.landeras@gmail.com>
2020-04-13 20:04:08 +0200

    Set storage packages, UI and client as preview version

--- src/HealthChecks.UI.SqlServer.Storage/HealthChecksUIBuilderExtensions.cs
+++ src/HealthChecks.UI.SqlServer.Storage/HealthChecksUIBuilderExtensions.cs
@@ -10,7 +10,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static HealthChecksUIBuilder AddSqlServerStorage(this HealthChecksUIBuilder builder, string connectionString, Action<DbContextOptionsBuilder> configureOptions = null)
         {            
             builder.Services.AddDbContext<HealthChecksDb>(options =>
-            {
+            {   
                 configureOptions?.Invoke(options);
                 options.UseSqlServer(connectionString, s => s.MigrationsAssembly("HealthChecks.UI.SqlServer.Storage"));
             });

41f45f1b74a62e053c951165ec2d5f73eace0879
Carlos Landeras <carlos.landeras@gmail.com>
2020-04-13 12:09:51 +0200

    Add Collector interceptors and storage providers API functional tests

---
+++ test/FunctionalTests/HealthChecks.UI/DatabaseProviders/HostBuilderHelper.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+using HealthChecks.UI.Client;
+using HealthChecks.UI.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace FunctionalTests.HealthChecks.UI.DatabaseProviders
+{
+    public class HostBuilderHelper
+    {
+        public static IWebHostBuilder Create(ManualResetEventSlim hostReset, ManualResetEventSlim collectorReset = null, Action<HealthChecksUIBuilder> configureUI = null)
+        {
+            return new WebHostBuilder()
+               .ConfigureServices(services =>
+               {
+                   var builder = services
+                   .AddRouting()
+                   .AddHealthChecks()
+                   .AddCheck("check1", () => HealthCheckResult.Healthy())
+                   .Services
+                   .AddHealthChecksUI(setup =>
+                   {
+                       setup.AddHealthCheckEndpoint(ProviderTestHelper.Endpoints[0].Name, ProviderTestHelper.Endpoints[0].Uri);
+                   });
+
+                   configureUI?.Invoke(builder);
+
+                   if(collectorReset != null)
+                   {
+                       services.AddTransient<IHealthCheckCollectorInterceptor>(sp => new TestCollectorInterceptor(collectorReset));
+                   }
+
+               }).Configure(app =>
+               {
+                   app
+                   .UseRouting()
+                   .UseEndpoints(endpoints =>
+                   {
+                       endpoints.MapHealthChecks("/health", new HealthCheckOptions
+                       {
+                           Predicate = r => true,
+                           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                       });
+                       endpoints.MapHealthChecksUI();
+                   });
+
+                   var lifetime = app.ApplicationServices.GetRequiredService<IHostApplicationLifetime>();
+                   lifetime.ApplicationStarted.Register(() =>
+                   {
+                       hostReset.Set();
+                   });
+               });
+        }
+    }
+}

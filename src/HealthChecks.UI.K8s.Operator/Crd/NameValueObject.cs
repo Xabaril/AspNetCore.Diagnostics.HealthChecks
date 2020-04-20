f6eaccf41647596afc0b138df99c890e2073f8b2
Carlos Landeras <carlos.landeras@gmail.com>
2020-04-20 16:39:23 +0200

    [k8s operator] Allow service and deployment annotations

---
+++ src/HealthChecks.UI.K8s.Operator/Crd/NameValueObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthChecks.UI.K8s.Operator.Crd
+{
+    public class NameValueObject
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}

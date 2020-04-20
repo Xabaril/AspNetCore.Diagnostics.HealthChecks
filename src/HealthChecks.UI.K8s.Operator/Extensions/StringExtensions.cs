bb20beaf63461e0a54675fd518559ed0a37d17ef
Carlos Landeras <carlos.landeras@gmail.com>
2020-04-20 20:51:01 +0200

    [K8s Operator] UI branding and ConfigMap handler

---
+++ src/HealthChecks.UI.K8s.Operator/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace System
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string str) => string.IsNullOrEmpty(str);
+        public static bool NotEmpty(this string str) => !IsEmpty(str);
+    }
+}

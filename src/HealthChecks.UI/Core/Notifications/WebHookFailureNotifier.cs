544e04a1d8518684901503e98ef2a7f545de7212
Carlos Landeras <carlos.landeras@gmail.com>
2020-04-23 10:02:29 +0200

    Escape all webhook payload fields

--- src/HealthChecks.UI/Core/Notifications/WebHookFailureNotifier.cs
+++ src/HealthChecks.UI/Core/Notifications/WebHookFailureNotifier.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace HealthChecks.UI.Core.Notifications
 {
@@ -73,9 +74,9 @@ namespace HealthChecks.UI.Core.Notifications
                     }
 
                     var payload = isHealthy ? webHook.RestoredPayload : webHook.Payload;
-                    payload = payload.Replace(Keys.LIVENESS_BOOKMARK, name)
-                        .Replace(Keys.FAILURE_BOOKMARK, failure)
-                        .Replace(Keys.DESCRIPTIONS_BOOKMARK, description);
+                    payload = payload.Replace(Keys.LIVENESS_BOOKMARK, HttpUtility.JavaScriptStringEncode(name))
+                        .Replace(Keys.FAILURE_BOOKMARK, HttpUtility.JavaScriptStringEncode(failure))
+                        .Replace(Keys.DESCRIPTIONS_BOOKMARK, HttpUtility.JavaScriptStringEncode(description));
 
 
                     Uri.TryCreate(webHook.Uri, UriKind.Absolute, out var absoluteUri);

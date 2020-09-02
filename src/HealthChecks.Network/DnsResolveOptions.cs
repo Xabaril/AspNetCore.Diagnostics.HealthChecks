e424418c0ca34fa3d81777a1268f347da6f100f5
Carlos Landeras <carlos.landeras@gmail.com>
2020-09-02 09:52:58 +0200

    Fix #615 - DnsHealthchecks AddHost method changed to internal and added documentation

--- src/HealthChecks.Network/DnsResolveOptions.cs
+++ src/HealthChecks.Network/DnsResolveOptions.cs
@@ -5,7 +5,7 @@ namespace HealthChecks.Network
     public class DnsResolveOptions
     {
         internal Dictionary<string, DnsRegistration> ConfigureHosts = new Dictionary<string, DnsRegistration>();
-        public void AddHost(string host, DnsRegistration registration)
+        internal void AddHost(string host, DnsRegistration registration)
         {
             ConfigureHosts.Add(host, registration);
         }

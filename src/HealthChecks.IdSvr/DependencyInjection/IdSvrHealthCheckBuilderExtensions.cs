1c0b93c875636d7a483b232ffafe8e24bbc5acc2
Twan Jacobs <AroglDarthu@users.noreply.github.com>
2020-09-18 16:51:21 +0200

    Add overload taking a uri provider

--- src/HealthChecks.IdSvr/DependencyInjection/IdSvrHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.IdSvr/DependencyInjection/IdSvrHealthCheckBuilderExtensions.cs
@@ -1,4 +1,4 @@
-﻿using HealthChecks.IdSvr;
+using HealthChecks.IdSvr;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
 using System.Collections.Generic;
@@ -21,7 +21,7 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </param>
         /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
         /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
-        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns></param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
         public static IHealthChecksBuilder AddIdentityServer(this IHealthChecksBuilder builder, Uri idSvrUri, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default,TimeSpan? timeout = default)
         {
             var registrationName = name ?? NAME;
@@ -34,6 +34,46 @@ namespace Microsoft.Extensions.DependencyInjection
                 failureStatus,
                 tags,
                 timeout));
+        }
+        /// <summary>
+        /// Add a health check for Identity Server.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="uriProvider">Factory for providing the uri of the Identity Server to check.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'idsvr' will be used for the name.</param>
+        /// <param name="failureStatus"></param>
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
+        public static IHealthChecksBuilder AddIdentityServer(
+            this IHealthChecksBuilder builder,
+            Func<IServiceProvider, Uri> uriProvider,
+            string name = null,
+            HealthStatus? failureStatus = null,
+            IEnumerable<string> tags = null,
+            TimeSpan? timeout = null)
+        {
+            builder.Services.AddHttpClient();
+            var registrationName = name ?? NAME;
+
+            return builder.Add(
+                new HealthCheckRegistration(
+                    registrationName,
+                    sp => new IdSvrHealthCheck(() => CreateIdentityServerHttpClient(sp, registrationName, uriProvider)),
+                    failureStatus,
+                    tags,
+                    timeout));
+        }
+        private static HttpClient CreateIdentityServerHttpClient(IServiceProvider sp, string registrationName, Func<IServiceProvider, Uri> uriProvider)
+        {
+            var authorityUri = uriProvider(sp);
+            var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+            var client = httpClientFactory.CreateClient(registrationName);
+            client.BaseAddress = authorityUri;
+
+            return client;
         }
     }
 }

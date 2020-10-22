60124a1aa314a1560390af26123572362111210a
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2020-10-22 18:10:20 +0200

    Changed the collection type to fix #655

--- src/HealthChecks.MongoDb/MongoDbHealthCheck.cs
+++ src/HealthChecks.MongoDb/MongoDbHealthCheck.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using MongoDB.Driver;
 using System;
 using System.Collections.Concurrent;
@@ -10,12 +10,16 @@ namespace HealthChecks.MongoDb
     public class MongoDbHealthCheck
         : IHealthCheck
     {
-        private static readonly ConcurrentDictionary<MongoClientSettings, MongoClient> _mongoClient = new ConcurrentDictionary<MongoClientSettings, MongoClient>();
+        private static readonly ConcurrentDictionary<string, MongoClient> _mongoClient = new ConcurrentDictionary<string, MongoClient>();
         private readonly MongoClientSettings _mongoClientSettings;
         private readonly string _specifiedDatabase;
         public MongoDbHealthCheck(string connectionString, string databaseName = default)
             : this(MongoClientSettings.FromUrl(MongoUrl.Create(connectionString)), databaseName)
         {
+            if (databaseName == default)
+            {
+                _specifiedDatabase = MongoUrl.Create(connectionString)?.DatabaseName;
+            }
         }
         public MongoDbHealthCheck(MongoClientSettings clientSettings, string databaseName = default)
         {
@@ -26,22 +30,23 @@ namespace HealthChecks.MongoDb
         {
             try
             {
-                var mongoClient = _mongoClient.GetOrAdd(_mongoClientSettings, settings => new MongoClient(settings));
+                var mongoClient = _mongoClient.GetOrAdd(_mongoClientSettings.ToString(), _ => new MongoClient(_mongoClientSettings));
 
                 if (!string.IsNullOrEmpty(_specifiedDatabase))
                 {
                     // some users can't list all databases depending on database privileges, with
-                    // this you can list only collection on specified database.
+                    // this you can list only collections on specified database.
                     // Related with issue #43
 
-                    await mongoClient
+                    using var cursor = await mongoClient
                         .GetDatabase(_specifiedDatabase)
-                        .ListCollectionsAsync(cancellationToken: cancellationToken);
+                        .ListCollectionNamesAsync(cancellationToken: cancellationToken);
+                    await cursor.FirstAsync(cancellationToken);
                 }
                 else
                 {
-                    await mongoClient
-                        .ListDatabasesAsync(cancellationToken);
+                    using var cursor = await mongoClient.ListDatabaseNamesAsync(cancellationToken);
+                    await cursor.FirstOrDefaultAsync(cancellationToken);
                 }
 
                 return HealthCheckResult.Healthy();

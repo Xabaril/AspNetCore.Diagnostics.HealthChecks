ee0880ce48948afedc5e0b6e85487a31e411ab86
smcavallo <smcavallo@hotmail.com>
2020-12-10 16:08:57 -0500

    backport HealthChecks.Publisher.Prometheus to .net-core-2.2 Signed-off-by: S.Cavallo <smcavallo@hotmail.com>

---
+++ src/HealthChecks.Prometheus.Metrics/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HealthChecks.Prometheus.Metrics;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class PrometheusHealthCheckMiddleware
+    {
+        public static IApplicationBuilder UseHealthChecksPrometheusExporter(this IApplicationBuilder applicationBuilder, PathString endpoint)
+        {
+            return applicationBuilder.UseHealthChecksPrometheusExporter(endpoint, configure: null);
+        }
+
+        public static IApplicationBuilder UseHealthChecksPrometheusExporter(this IApplicationBuilder applicationBuilder, PathString endpoint, Action<HealthCheckOptions> configure)
+        {
+            var options = new HealthCheckOptions
+            {
+                ResponseWriter = PrometheusResponseWriter.WritePrometheusResultText
+            };
+            configure?.Invoke(options);
+
+            applicationBuilder.UseHealthChecks(endpoint, options);
+            return applicationBuilder;
+        }
+    }
+}

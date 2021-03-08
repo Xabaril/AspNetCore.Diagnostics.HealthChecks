d6fac3ed2fd70bc2f723675ec1544c074efba2ef
Unai Zorrilla Castro <unai@plainconcepts.com>
2021-03-08 12:10:28 +0100

    Added support for address family

--- test/FunctionalTests/HealthCheck.Network/TcpHealthCheckTests.cs
+++ test/FunctionalTests/HealthCheck.Network/TcpHealthCheckTests.cs
@@ -3,6 +3,7 @@ using HealthChecks.Network;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,7 +18,9 @@ namespace FunctionalTests.HealthChecks.Network
         public async Task respect_configured_timeout_and_throw_operation_cancelled_exception()
         {
             var options = new TcpHealthCheckOptions();
+
             options.AddHost("invalid", 5555);
+            options.AddressFamily = AddressFamily.InterNetworkV6;
 
             var tcpHealthCheck = new TcpHealthCheck(options);
 

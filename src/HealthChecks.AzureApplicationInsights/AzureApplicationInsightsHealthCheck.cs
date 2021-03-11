982312f1a9a8815a28634d25e09be8411b55f8bc
Paterson Deshommes <pdeshommes@GENETEC.COM>
2021-03-10 21:43:34 -0500

    added new AzureApplicationInsights project with associated unit test project

---
+++ src/HealthChecks.AzureApplicationInsights/AzureApplicationInsightsHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecks.AzureApplicationInsights
+{
+    public class AzureApplicationInsightsHealthCheck : IHealthCheck
+    {
+        private readonly string _instrumentationKey;
+        public AzureApplicationInsightsHealthCheck(string instrumentationKey)
+        {
+            if(string.IsNullOrWhiteSpace(instrumentationKey))
+            {
+                throw new ArgumentNullException(nameof(instrumentationKey));
+            }
+
+            _instrumentationKey = instrumentationKey;         
+        }
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

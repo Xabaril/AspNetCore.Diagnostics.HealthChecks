c41233161890315ee9d24873a87e6767cf150efe
Paterson Deshommes <pdeshommes@GENETEC.COM>
2021-03-10 22:25:37 -0500

    added logic for azure application insights health check

--- src/HealthChecks.AzureApplicationInsights/AzureApplicationInsightsHealthCheck.cs
+++ src/HealthChecks.AzureApplicationInsights/AzureApplicationInsightsHealthCheck.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,7 +8,15 @@ namespace HealthChecks.AzureApplicationInsights
 {
     public class AzureApplicationInsightsHealthCheck : IHealthCheck
     {
+        //from https://docs.microsoft.com/en-us/azure/azure-monitor/app/ip-addresses#outgoing-ports
+        private readonly string[] m_appInsightsUrls =
+            {
+            "https://dc.applicationinsights.azure.com",
+            "https://dc.applicationinsights.microsoft.com",
+            "https://dc.services.visualstudio.com"
+            };
         private readonly string _instrumentationKey;
+
         public AzureApplicationInsightsHealthCheck(string instrumentationKey)
         {
             if(string.IsNullOrWhiteSpace(instrumentationKey))
@@ -17,9 +26,50 @@ namespace HealthChecks.AzureApplicationInsights
 
             _instrumentationKey = instrumentationKey;         
         }
-        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await CheckHealthAsyncInternal(context,cancellationToken);
+            }
+            catch(Exception ex)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+            }
+        }
+        private async Task<HealthCheckResult> CheckHealthAsyncInternal(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            string path = $"/api/profiles/{_instrumentationKey}/appId";
+            Exception ex = null;
+            int index = 0;
+
+            while (index < m_appInsightsUrls.Length)
+            {
+                using(var httpClient = new HttpClient())
+                {
+                    try
+                    {
+                        var uri = new Uri(m_appInsightsUrls[index++] + path);
+                        HttpResponseMessage response = await httpClient.GetAsync(uri,cancellationToken);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return HealthCheckResult.Healthy();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        ex = e;
+                    }
+                }
+            }
+
+            if(ex == null)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, description: "Could not find application insights resource");
+            }
+
+            throw ex;
         }
     }
 }

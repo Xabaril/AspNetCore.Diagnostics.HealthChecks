ffb39e2b5470e605910e0e9dc13c48a5a2b1f3c1
Jakub Riha <jakubriha@outlook.com>
2021-08-08 16:21:30 +0200

    Add dependency injection unit tests

---
+++ test/UnitTests/DependencyInjection/S3/S3UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using Amazon.S3;
+using FluentAssertions;
+using HealthChecks.Aws.S3;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using Xunit;
+
+namespace UnitTests.HealthChecks.DependencyInjection.S3
+{
+    public class s3_registration_should
+    {
+        [Fact]
+        public void add_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddS3(_ => { _.S3Config = new AmazonS3Config(); });
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("aws s3");
+            check.GetType().Should().Be(typeof(S3HealthCheck));
+        }
+        [Fact]
+        public void add_named_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddS3(_ => { _.S3Config = new AmazonS3Config(); }, name: "my-s3-group");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("my-s3-group");
+            check.GetType().Should().Be(typeof(S3HealthCheck));
+        }
+    }
+}

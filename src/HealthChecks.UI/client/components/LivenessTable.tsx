44145b30e8e404c23d7d6e182e65e0240a8de69a
Bo Petersen <bo.soborg.petersen@3shape.com>
2021-09-17 10:00:25 +0200

    Fix bug with regression of datetime string in OnStateFrom column in UI

--- src/HealthChecks.UI/client/components/LivenessTable.tsx
+++ src/HealthChecks.UI/client/components/LivenessTable.tsx
@@ -1,4 +1,5 @@
 ﻿import React, { FunctionComponent } from 'react';
+import moment from 'moment';
 import { Liveness } from '../typings/models';
 import { discoveryServices, getStatusConfig } from '../healthChecksResources';
 import { CheckTable } from './CheckTable';
@@ -91,7 +92,9 @@ const LivenessTable: FunctionComponent<LivenessTableProps> = ({ livenessData, ex
                     {statusConfig!.image}
                   </i>
                 </td>
-                <td>{item.onStateFrom}</td>
+                <td className="align-center">
+                  {item.status} {moment.utc(item.onStateFrom).fromNow().toString()}
+                </td>
                 <td className="align-center">
                   {new Date(item.lastExecuted).toLocaleString()}
                 </td>

39273434d1494f467bfa86e9bdc4bbf13e8b2c25
Ivan Maximov <sungam3r@yandex.ru>
2021-11-06 20:09:11 +0300

    Update RegistrationTests.cs

--- test/HealthChecks.IdSvr.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.IdSvr.Tests/DependencyInjection/RegistrationTests.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;

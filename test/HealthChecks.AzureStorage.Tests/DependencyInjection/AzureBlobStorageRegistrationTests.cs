e334e2b0960c691f4f11047bd5c85624b39e70ce
Carlos Recuero Díaz <crecuero@plainconcepts.com>
2021-11-22 19:12:39 +0100

    Add AzureKeyVault and AzureStorage tests and CI/CD

---
+++ test/HealthChecks.AzureStorage.Tests/DependencyInjection/AzureBlobStorageRegistrationTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using HealthChecks.AzureStorage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using Xunit;
+
+namespace HealthChecks.AzureStorage.Tests.DependencyInjection
+{
+    public class azureblobstorage_registration_should
+    {
+        [Fact]
+        public void add_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddAzureBlobStorage("the-connection-string");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("azureblob");
+            check.GetType().Should().Be(typeof(AzureBlobStorageHealthCheck));
+        }
+        [Fact]
+        public void add_named_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddAzureBlobStorage("the-connection-string", name: "my-azureblob-group");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("my-azureblob-group");
+            check.GetType().Should().Be(typeof(AzureBlobStorageHealthCheck));
+        }
+        [Fact]
+        public void add_named_container_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddAzureBlobStorage("the-connection-string", containerName: "container");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("azureblob");
+            check.GetType().Should().Be(typeof(AzureBlobStorageHealthCheck));
+        }
+    }
+}

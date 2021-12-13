59e90e3bf2e98562a3fff5a35516e9f0f7c3111c
Carlos Recuero Díaz <crecuero@plainconcepts.com>
2021-12-13 18:24:07 +0100

    Publisher Datadog and Seq

---
+++ test/HealthChecks.Publisher.Datadog.Tests/DependencyInjection/RegistrationTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Xunit;
+
+namespace HealthChecks.Publisher.Datadog.Tests.DependencyInjection
+{
+    public class datadog_publisher_registration_should
+    {
+        [Fact]
+        public void add_healthcheck_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services
+                .AddHealthChecks()
+                .AddDatadogPublisher(serviceCheckName: "serviceCheckName", datadogAgentName: "agentName");
+
+            var serviceProvider = services.BuildServiceProvider();
+            var publisher = serviceProvider.GetService<IHealthCheckPublisher>();
+
+            Assert.NotNull(publisher);
+        }
+    }
+}

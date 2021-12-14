ba818d13b5a063d1d0a23052b41d2dab9a578516
Carlos Recuero Díaz <crecuero@plainconcepts.com>
2021-12-14 08:28:41 +0100

    SignalR SendGrid CI/CD

--- test/HealthChecks.SendGrid.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.SendGrid.Tests/DependencyInjection/RegistrationTests.cs
@@ -1,15 +1,14 @@
-﻿using System.Linq;
-using FluentAssertions;
+﻿using FluentAssertions;
 using FluentAssertions.Execution;
-using HealthChecks.SendGrid;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
+using System.Linq;
 using Xunit;
 
-namespace UnitTests.HealthChecks.DependencyInjection.SendGrid
+namespace HealthChecks.SendGrid.Tests.DependencyInjection
 {
-    public class sendgrid_registrations_should
+    public class sendgrid_registration_should
     {
         [Fact]
         public void add_health_check_when_properly_configured()

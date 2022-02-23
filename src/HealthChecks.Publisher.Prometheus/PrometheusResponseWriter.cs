d99bd1186061fabe59ad0888f15388d84835fa70
Ivan Maximov <sungam3r@yandex.ru>
2022-02-23 17:09:35 +0300

    fix

--- src/HealthChecks.Publisher.Prometheus/PrometheusResponseWriter.cs
+++ src/HealthChecks.Publisher.Prometheus/PrometheusResponseWriter.cs
@@ -1,6 +1,6 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using System.Threading.Tasks;
 
 namespace HealthChecks.Publisher.Prometheus
 {

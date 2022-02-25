3c5ec9415ad9fd32503a5d6eba7d643111e33363
Ivan Maximov <sungam3r@yandex.ru>
2022-02-25 03:47:54 +0300

    Optimize UIResponseWriter

--- src/HealthChecks.UI.Core/UIHealthReport.cs
+++ src/HealthChecks.UI.Core/UIHealthReport.cs
@@ -6,8 +6,8 @@ using System.Linq;
 namespace HealthChecks.UI.Core
 {
     /*
-     * Models for UI Client. This models represent a indirection between HealthChecks API and 
-     * UI Client in order to implement some features not present on HealthChecks of substitute 
+     * Models for UI Client. This models represent a indirection between HealthChecks API and
+     * UI Client in order to implement some features not present on HealthChecks of substitute
      * some properties etc.
      */
     public class UIHealthReport
@@ -21,6 +21,7 @@ namespace HealthChecks.UI.Core
             Entries = entries ?? new Dictionary<string, UIHealthReportEntry>();
             TotalDuration = totalDuration;
         }
+
         public static UIHealthReport CreateFrom(HealthReport report)
         {
             var uiReport = new UIHealthReport(new Dictionary<string, UIHealthReportEntry>(), report.TotalDuration)
@@ -40,9 +41,7 @@ namespace HealthChecks.UI.Core
 
                 if (item.Value.Exception != null)
                 {
-                    var message = item.Value.Exception?
-                        .Message
-                        .ToString();
+                    var message = item.Value.Exception?.Message;
 
                     entry.Exception = message;
                     entry.Description = item.Value.Description ?? message;
@@ -55,6 +54,7 @@ namespace HealthChecks.UI.Core
 
             return uiReport;
         }
+
         public static UIHealthReport CreateFrom(Exception exception, string entryName = "Endpoint")
         {
             var uiReport = new UIHealthReport(new Dictionary<string, UIHealthReportEntry>(), TimeSpan.FromSeconds(0))
@@ -73,12 +73,14 @@ namespace HealthChecks.UI.Core
             return uiReport;
         }
     }
+
     public enum UIHealthStatus
     {
         Unhealthy = 0,
         Degraded = 1,
         Healthy = 2
     }
+
     public class UIHealthReportEntry
     {
         public IReadOnlyDictionary<string, object> Data { get; set; }

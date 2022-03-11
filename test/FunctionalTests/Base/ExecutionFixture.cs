6477b57446ba7a9838ccb7b73da39aa2ea25d593
Gabriel Monteagudo <gabrielmaldi@hotmail.com>
2022-03-11 18:19:19 -0300

    Revert "Remove obsolete FunctionalTests"
    This reverts commit 9cbedfde1c156f10e224d002314c86460b0baeaf.

---
+++ test/FunctionalTests/Base/ExecutionFixture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FunctionalTests.Base
+{
+    public class ExecutionFixture
+    {
+        public bool IsAppVeyorExecution { get; private set; }
+
+        public ExecutionFixture()
+        {
+            IsAppVeyorExecution = Environment.GetEnvironmentVariable("Appveyor")?.ToUpperInvariant() == "TRUE";
+        }
+    }
+}
---
+++ test/FunctionalTests/Base/TestCollectorInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using HealthChecks.UI.Core;
+using HealthChecks.UI.Core.Data;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunctionalTests.Base
+{
+    internal class TestCollectorInterceptor : IHealthCheckCollectorInterceptor
+    {
+        private readonly ManualResetEventSlim _resetEvent;
+
+        public TestCollectorInterceptor(ManualResetEventSlim resetEvent)
+        {
+            _resetEvent = resetEvent ?? throw new ArgumentNullException(nameof(resetEvent));
+        }
+
+        public ValueTask OnCollectExecuted(UIHealthReport report)
+        {
+            _resetEvent.Set();
+            return new ValueTask();
+        }
+
+        public ValueTask OnCollectExecuting(HealthCheckConfiguration healthCheck)
+        {
+            return new ValueTask();
+        }
+    }
+}

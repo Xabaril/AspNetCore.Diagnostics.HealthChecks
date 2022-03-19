f0a9c333e0806c9b9ad498fe28b823d5edfeb28f
Ivan Maximov <sungam3r@yandex.ru>
2022-03-19 10:22:00 +0300

    Fix oracle tests (#1021)
    Fix testing

--- test/HealthChecks.Oracle.Tests/Functional/OracleHealthCheckTests.cs
+++ test/HealthChecks.Oracle.Tests/Functional/OracleHealthCheckTests.cs
@@ -15,7 +15,7 @@ namespace HealthChecks.Oracle.Tests.Functional
         [Fact]
         public async Task be_healthy_when_oracle_is_available()
         {
-            var connectionString = "Data Source=localhost:1521/xe;User Id=system;Password=oracle";
+            var connectionString = "Data Source=localhost:1521/XEPDB1;User Id=system;Password=oracle";
 
             var webHostBuilder = new WebHostBuilder()
                 .ConfigureServices(services =>
@@ -36,13 +36,13 @@ namespace HealthChecks.Oracle.Tests.Functional
             var response = await server.CreateRequest("/health")
                 .GetAsync();
 
-            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK, await response.Content.ReadAsStringAsync());
         }
 
         [Fact]
         public async Task be_unhealthy_when_oracle_is_not_available()
         {
-            var connectionString = "Data Source=255.255.255.255:1521/xe;User Id=system;Password=oracle";
+            var connectionString = "Data Source=255.255.255.255:1521/XEPDB1;User Id=system;Password=oracle";
 
             var webHostBuilder = new WebHostBuilder()
                 .ConfigureServices(services =>
@@ -70,7 +70,7 @@ namespace HealthChecks.Oracle.Tests.Functional
         [Fact]
         public async Task be_unhealthy_when_sql_query_is_not_valid()
         {
-            var connectionString = "Data Source=localhost:1521/xe;User Id=system;Password=oracle";
+            var connectionString = "Data Source=localhost:1521/XEPDB1;User Id=system;Password=oracle";
             var webHostBuilder = new WebHostBuilder()
                 .ConfigureServices(services =>
                 {
@@ -98,7 +98,7 @@ namespace HealthChecks.Oracle.Tests.Functional
         public async Task be_healthy_with_connection_string_factory_when_oracle_is_available()
         {
             bool factoryCalled = false;
-            string connectionString = "Data Source=localhost:1521/xe;User Id=system;Password=oracle";
+            string connectionString = "Data Source=localhost:1521/XEPDB1;User Id=system;Password=oracle";
 
             var webHostBuilder = new WebHostBuilder()
                 .ConfigureServices(services =>

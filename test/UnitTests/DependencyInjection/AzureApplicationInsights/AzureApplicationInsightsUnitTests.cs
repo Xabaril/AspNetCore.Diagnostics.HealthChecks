f9211b10b8de8badd31387f61223c7799633dac7
paterson-deshommes <paterson-deshommes@users.noreply.github.com>
2022-04-18 12:36:43 -0400

    Update test/UnitTests/DependencyInjection/AzureApplicationInsights/AzureApplicationInsightsUnitTests.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- test/UnitTests/DependencyInjection/AzureApplicationInsights/AzureApplicationInsightsUnitTests.cs
+++ test/UnitTests/DependencyInjection/AzureApplicationInsights/AzureApplicationInsightsUnitTests.cs
@@ -54,7 +54,7 @@ namespace UnitTests.DependencyInjection.AzureApplicationInsights
                 .AddAzureApplicationInsights(string.Empty);
 
             using var serviceProvider = services.BuildServiceProvider();
-            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
 
             var registration = options.Value.Registrations.First();
 

49c594befca528920eed9a64366be2e9c5982c5a
Paterson Deshommes <pat14_14@hotmail.com>
2022-04-18 15:29:45 -0400

    fix some build errors

--- src/HealthChecks.AzureApplicationInsights/DependencyInjection/AzureApplicationInsightsHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.AzureApplicationInsights/DependencyInjection/AzureApplicationInsightsHealthCheckBuilderExtensions.cs
@@ -22,11 +22,11 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
         /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
         /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
-        public static IHealthChecksBuilder AddAzureApplicationInsights(this IHealthChecksBuilder builder, string instrumentationKey, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default, TimeSpan? timeout = default)
+        public static IHealthChecksBuilder AddAzureApplicationInsights(this IHealthChecksBuilder builder, string instrumentationKey, string? name = default, HealthStatus? failureStatus = default, IEnumerable<string>? tags = default, TimeSpan? timeout = default)
         {
               return builder.Add(new HealthCheckRegistration(
                   name ?? AZUREAPPLICATIONINSIGHTS_NAME,
-                  sp => new AzureApplicationInsightsHealthCheck(instrumentationKey),
+                  sp => new AzureApplicationInsightsHealthCheck(instrumentationKey, sp.GetRequiredService<IHttpClientFactory>()),
                   failureStatus,
                   tags,
                   timeout));

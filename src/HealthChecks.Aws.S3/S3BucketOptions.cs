1beaf06578dcf8a8dfe466cb5c0528f80c5abd6a
Joakim Lindqvist <jocco@slackerstudios.se>
2022-04-19 13:39:51 +0200

    Apply suggestions from code review
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/HealthChecks.Aws.S3/S3BucketOptions.cs
+++ src/HealthChecks.Aws.S3/S3BucketOptions.cs
@@ -6,11 +6,11 @@ namespace HealthChecks.Aws.S3
 {
     public class S3BucketOptions
     {
-        public AWSCredentials? Credentials { get; set; } = null;
+        public AWSCredentials? Credentials { get; set; }
 
-        [Obsolete("Specify access key and secret as a BasicCredential to AWSCredentials instead")]
+        [Obsolete("Specify access key and secret as a BasicCredential to Credentials property instead")]
         public string? AccessKey { get; set; }
-        [Obsolete("Specify access key and secret as a BasicCredential to AWSCredentials instead")]
+        [Obsolete("Specify access key and secret as a BasicCredential to Credentials property instead")]
         public string? SecretKey { get; set; }
 
         public AmazonS3Config S3Config { get; set; } = null!;

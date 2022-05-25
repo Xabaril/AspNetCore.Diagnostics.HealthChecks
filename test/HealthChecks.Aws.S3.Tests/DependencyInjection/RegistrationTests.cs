babff3088620a4fa14faad95df703fcf5c8446a1
zodraz <zodraz@gmail.com>
2022-05-25 19:24:16 +0200

    Fixing tests

--- test/HealthChecks.Aws.S3.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.Aws.S3.Tests/DependencyInjection/RegistrationTests.cs
@@ -1,51 +1,53 @@
 using Amazon.S3;
 
-namespace HealthChecks.Aws.S3.Tests.DependencyInjection;
-public class aws_s3_registration_should
+namespace HealthChecks.Aws.S3.Tests.DependencyInjection
 {
-    [Fact]
-    public void add_health_check_when_properly_configured()
+    public class aws_s3_registration_should
     {
-        var services = new ServiceCollection();
-        services.AddHealthChecks()
-            .AddS3(options =>
-            {
-                options.AccessKey = "access-key";
-                options.BucketName = "bucket-name";
-                options.SecretKey = "secret-key";
-                options.S3Config = new AmazonS3Config();
-            });
-
-        using var serviceProvider = services.BuildServiceProvider();
-        var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
-
-        var registration = options.Value.Registrations.First();
-        var check = registration.Factory(serviceProvider);
-
-        registration.Name.Should().Be("aws s3");
-        check.GetType().Should().Be(typeof(S3HealthCheck));
-    }
-
-    [Fact]
-    public void add_named_health_check_when_properly_configured()
-    {
-        var services = new ServiceCollection();
-        services.AddHealthChecks()
+        [Fact]
+        public void add_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
                 .AddS3(options =>
                 {
                     options.AccessKey = "access-key";
                     options.BucketName = "bucket-name";
                     options.SecretKey = "secret-key";
                     options.S3Config = new AmazonS3Config();
-                }, name: "aws s3 check");
+                });
 
-        using var serviceProvider = services.BuildServiceProvider();
-        var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
 
-        var registration = options.Value.Registrations.First();
-        var check = registration.Factory(serviceProvider);
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
 
-        registration.Name.Should().Be("aws s3 check");
-        check.GetType().Should().Be(typeof(S3HealthCheck));
+            registration.Name.Should().Be("aws s3");
+            check.GetType().Should().Be(typeof(S3HealthCheck));
+        }
+
+        [Fact]
+        public void add_named_health_check_when_properly_configured()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                 .AddS3(options =>
+                 {
+                     options.AccessKey = "access-key";
+                     options.BucketName = "bucket-name";
+                     options.SecretKey = "secret-key";
+                     options.S3Config = new AmazonS3Config();
+                 }, name: "aws s3 check");
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("aws s3 check");
+            check.GetType().Should().Be(typeof(S3HealthCheck));
+        }
     }
 }

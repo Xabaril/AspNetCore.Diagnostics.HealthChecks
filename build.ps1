3cbaa004011d1267bc76ba010c3e945a10763b94
zodraz <zodraz@users.noreply.github.com>
2022-05-27 23:03:57 +0200

    Aws secrets manager (#1126)

--- build.ps1
+++ build.ps1
@@ -115,6 +115,7 @@ if ($suffix -eq "") {
     exec { & dotnet pack .\src\HealthChecks.ArangoDb\HealthChecks.ArangoDb.csproj -c Release -o .\artifacts --include-symbols --no-build }
     exec { & dotnet pack .\src\HealthChecks.Nats\HealthChecks.Nats.csproj -c Release -o .\artifacts --include-symbols --no-build }
     exec { & dotnet pack .\src\HealthChecks.AzureDigitalTwin\HealthChecks.AzureDigitalTwin.csproj -c Release -o .\artifacts --include-symbols --no-build }
+    exec { & dotnet pack .\src\HealthChecks.Aws.SecretsManager\HealthChecks.Aws.SecretsManager.csproj -c Release -o .\artifacts --include-symbols --no-build }
 }
 
 else {
@@ -167,4 +168,5 @@ else {
     exec { & dotnet pack .\src\HealthChecks.ArangoDb\HealthChecks.ArangoDb.csproj -c Release -o .\artifacts --include-symbols --no-build --version-suffix=$suffix }
     exec { & dotnet pack .\src\HealthChecks.Nats\HealthChecks.Nats.csproj -c Release -o .\artifacts --include-symbols --no-build --version-suffix=$suffix }
     exec { & dotnet pack .\src\HealthChecks.AzureDigitalTwin\HealthChecks.AzureDigitalTwin.csproj -c Release -o .\artifacts --include-symbols --no-build --version-suffix=$suffix }
+    exec { & dotnet pack .\src\HealthChecks.Aws.SecretsManager\HealthChecks.Aws.SecretsManager.csproj -c Release -o .\artifacts --include-symbols --no-build --version-suffix=$suffix }
 }

f9b87b54171256365fb58dd7632e52db4fa80a66
Rob <19424841+roblohmann@users.noreply.github.com>
2022-06-01 11:47:10 +0200

    Update src/HealthChecks.Uris/UriHealthCheck.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/HealthChecks.Uris/UriHealthCheck.cs
+++ src/HealthChecks.Uris/UriHealthCheck.cs
@@ -57,7 +57,7 @@ namespace HealthChecks.Uris
                             return new HealthCheckResult(context.Registration.FailureStatus, description: $"Discover endpoint #{idx} is not responding with code in {expectedStatusCodes.Min}...{expectedStatusCodes.Max} range, the current status is {response.StatusCode}.");
                         }
 
-                        if (!string.IsNullOrWhiteSpace(item.ExpectedContent))
+                        if (item.ExpectedContent != null)
                         {
                             var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                             if (responseBody != item.ExpectedContent)

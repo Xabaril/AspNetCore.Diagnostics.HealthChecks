1482e93489defdb5260095fd5132903fbc061bbf
levimatheri <levimatheri@gmail.com>
2022-07-03 14:29:39 -0700

    Add extension for CosmosDb/TableService that accepts TokenCredential for authentication (#1316)

--- test/HealthChecks.CosmosDb.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.CosmosDb.Tests/DependencyInjection/RegistrationTests.cs
@@ -1,3 +1,6 @@
+using Azure.Core;
+using Azure.Data.Tables;
+
 namespace HealthChecks.CosmosDb.Tests.DependencyInjection
 {
     public class cosmosdb_registration_should
@@ -8,6 +11,22 @@ namespace HealthChecks.CosmosDb.Tests.DependencyInjection
             var services = new ServiceCollection();
             services.AddHealthChecks()
                 .AddCosmosDb("myconnectionstring");
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("cosmosdb");
+            check.GetType().Should().Be(typeof(CosmosDbHealthCheck));
+        }
+        [Fact]
+        public void add_cosmosdb_health_check_when_properly_configured_using_token_credential()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCosmosDb("cosmosdbaccounturi", new MockTokenCredential());
 
             using var serviceProvider = services.BuildServiceProvider();
             var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
@@ -35,11 +54,43 @@ namespace HealthChecks.CosmosDb.Tests.DependencyInjection
             check.GetType().Should().Be(typeof(CosmosDbHealthCheck));
         }
         [Fact]
+        public void add_cosmosdb_health_check_when_properly_configured_using_token_credential_with_database()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCosmosDb("cosmosdbaccounturi", new MockTokenCredential(), "databasename");
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("cosmosdb");
+            check.GetType().Should().Be(typeof(CosmosDbHealthCheck));
+        }
+        [Fact]
         public void add_cosmosdb_health_check_when_properly_configured_with_database_and_collections()
         {
             var services = new ServiceCollection();
             services.AddHealthChecks()
                 .AddCosmosDbCollection("myconnectionstring", "dabasename", collections: new[] { "first-collection", "second_collections" });
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("cosmosdb");
+            check.GetType().Should().Be(typeof(CosmosDbHealthCheck));
+        }
+        [Fact]
+        public void add_cosmosdb_health_check_when_properly_configured_using_token_credential_with_database_and_collections()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddCosmosDbCollection("cosmosdbaccounturi", new MockTokenCredential(), "databasename", collections: new[] { "first-collection", "second_collections" });
 
             using var serviceProvider = services.BuildServiceProvider();
             var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
@@ -100,6 +151,38 @@ namespace HealthChecks.CosmosDb.Tests.DependencyInjection
             check.GetType().Should().Be(typeof(TableServiceHealthCheck));
         }
         [Fact]
+        public void add_azuretable_health_check_when_properly_configured_using_table_shared_key_credential()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddAzureTable(new Uri("http://localhost"), new TableSharedKeyCredential("mystorageaccount", "dGVzdEtleQ=="), "tableName");
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("azuretable");
+            check.GetType().Should().Be(typeof(TableServiceHealthCheck));
+        }
+        [Fact]
+        public void add_azuretable_health_check_when_properly_configured_using_token_credential()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddAzureTable(new Uri("http://localhost"), new MockTokenCredential(), "tableName");
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("azuretable");
+            check.GetType().Should().Be(typeof(TableServiceHealthCheck));
+        }
+        [Fact]
         public void add_azuretable_named_health_check_when_properly_configured()
         {
             var services = new ServiceCollection();
@@ -114,6 +197,19 @@ namespace HealthChecks.CosmosDb.Tests.DependencyInjection
 
             registration.Name.Should().Be("my-azuretable-group");
             check.GetType().Should().Be(typeof(TableServiceHealthCheck));
+        }
+    }
+
+    public class MockTokenCredential : TokenCredential
+    {
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
     }
 }

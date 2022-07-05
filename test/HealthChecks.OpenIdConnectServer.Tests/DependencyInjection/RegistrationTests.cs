b3efef8abf283173142a6e5c958a530736e8c24d
Twan Jacobs <AroglDarthu@users.noreply.github.com>
2022-07-05 14:17:57 +0200

    Idsvr uriprovider (#637)

--- test/HealthChecks.OpenIdConnectServer.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.OpenIdConnectServer.Tests/DependencyInjection/RegistrationTests.cs
@@ -34,5 +34,37 @@ namespace HealthChecks.IdSvr.Tests.DependencyInjection
             registration.Name.Should().Be("my-idsvr-group");
             check.GetType().Should().Be(typeof(IdSvrHealthCheck));
         }
+        [Fact]
+        public void add_health_check_when_properly_configured_with_uri_provider()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddIdentityServer(sp => new Uri("http://myidsvr"));
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("idsvr");
+            check.GetType().Should().Be(typeof(IdSvrHealthCheck));
+        }
+        [Fact]
+        public void add_named_health_check_when_properly_configured_with_uri_provider()
+        {
+            var services = new ServiceCollection();
+            services.AddHealthChecks()
+                .AddIdentityServer(sp => new Uri("http://myidsvr"), name: "my-idsvr-group");
+
+            using var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+            var registration = options.Value.Registrations.First();
+            var check = registration.Factory(serviceProvider);
+
+            registration.Name.Should().Be("my-idsvr-group");
+            check.GetType().Should().Be(typeof(IdSvrHealthCheck));
+        }
     }
 }

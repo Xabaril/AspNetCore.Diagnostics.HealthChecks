ad407e6218e6810fd2d85d64227e513fc61fbf50
Ivan Maximov <sungam3r@yandex.ru>
2022-08-08 09:12:46 +0300

    Convert some projects to file-scoped namespaces (#1380)

--- src/HealthChecks.Nats/NatsOptions.cs
+++ src/HealthChecks.Nats/NatsOptions.cs
@@ -1,31 +1,30 @@
-namespace HealthChecks.Nats
+namespace HealthChecks.Nats;
+
+/// <summary>
+/// Options for <see cref="NatsHealthCheck"/>.
+/// </summary>
+public class NatsOptions
 {
     /// <summary>
-    /// Options for <see cref="NatsHealthCheck"/>.
+    /// A string containing the URL (or URLs) to the NATS Server.
     /// </summary>
-    public class NatsOptions
-    {
-        /// <summary>
-        /// A string containing the URL (or URLs) to the NATS Server.
-        /// </summary>
-        public string Url { get; set; } = "nats://demo.nats.io:4222";
+    public string Url { get; set; } = "nats://demo.nats.io:4222";
 
-        /// <summary>
-        /// The full path to a chained credentials file.
-        /// </summary>
-        public string? CredentialsPath { get; set; }
+    /// <summary>
+    /// The full path to a chained credentials file.
+    /// </summary>
+    public string? CredentialsPath { get; set; }
 
-        /// <summary>
-        /// The path to a user's public JWT credentials.
-        /// </summary>
-        public string? Jwt { get; set; }
+    /// <summary>
+    /// The path to a user's public JWT credentials.
+    /// </summary>
+    public string? Jwt { get; set; }
 
-        /// <summary>
-        /// The path to a file for user's private Nkey seed.
-        /// </summary>
-        /// <remarks>
-        /// <see href="https://docs.nats.io/running-a-nats-service/configuration/securing_nats/auth_intro/nkey_auth"/>
-        /// </remarks>
-        public string? PrivateNKey { get; set; }
-    }
+    /// <summary>
+    /// The path to a file for user's private Nkey seed.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.nats.io/running-a-nats-service/configuration/securing_nats/auth_intro/nkey_auth"/>
+    /// </remarks>
+    public string? PrivateNKey { get; set; }
 }

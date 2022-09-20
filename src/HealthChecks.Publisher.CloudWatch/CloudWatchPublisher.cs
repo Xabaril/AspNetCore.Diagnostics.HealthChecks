313e6ce69ae487272bc196edf544b7d4bbcc4344
William Keller <william.keller@fulltechpro.com.br>
2022-09-20 17:04:03 -0300

    fix: File-scoped namespaces for all new code

--- src/HealthChecks.Publisher.CloudWatch/CloudWatchPublisher.cs
+++ src/HealthChecks.Publisher.CloudWatch/CloudWatchPublisher.cs
@@ -4,119 +4,119 @@ using Amazon.CloudWatch;
 using Amazon.CloudWatch.Model;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-namespace HealthChecks.Publisher.CloudWatch
+namespace HealthChecks.Publisher.CloudWatch;
+
+/// <summary>
+/// A health check publisher for AWS CloudWatch.
+/// </summary>
+internal class CloudWatchPublisher : IHealthCheckPublisher, IDisposable
 {
+    private readonly List<Dimension> _dimensions;
+    private readonly AmazonCloudWatchClient _amazonCloudWatchClient;
+
     /// <summary>
-    /// A health check publisher for AWS CloudWatch.
+    /// CloudWatchPublisher constructor
     /// </summary>
-    internal class CloudWatchPublisher : IHealthCheckPublisher, IDisposable
+    public CloudWatchPublisher()
     {
-        private readonly List<Dimension> _dimensions;
-        private readonly AmazonCloudWatchClient _amazonCloudWatchClient;
+        _amazonCloudWatchClient = new AmazonCloudWatchClient();
 
-        /// <summary>
-        /// CloudWatchPublisher constructor
-        /// </summary>
-        public CloudWatchPublisher()
-        {
-            _amazonCloudWatchClient = new AmazonCloudWatchClient();
+        var serviceCheckName = Assembly.GetEntryAssembly()?.GetName()?.Name ?? "undefined";
 
-            var serviceCheckName = Assembly.GetEntryAssembly()?.GetName()?.Name ?? "undefined";
-
-            _dimensions = new List<Dimension> {
+        _dimensions = new List<Dimension> {
                 new Dimension
                 {
                     Name = serviceCheckName,
                     Value = serviceCheckName
                 }
             };
-        }
+    }
 
-        /// <summary>
-        /// CloudWatchPublisher constructor
-        /// </summary>
-        /// <param name="serviceCheckName"></param>
-        public CloudWatchPublisher(string serviceCheckName) : this()
-        {
-            if (string.IsNullOrEmpty(serviceCheckName))
-                throw new ArgumentNullException(nameof(serviceCheckName));
+    /// <summary>
+    /// CloudWatchPublisher constructor
+    /// </summary>
+    /// <param name="serviceCheckName"></param>
+    public CloudWatchPublisher(string serviceCheckName) : this()
+    {
+        if (string.IsNullOrEmpty(serviceCheckName))
+            throw new ArgumentNullException(nameof(serviceCheckName));
 
-            _dimensions = new List<Dimension> {
+        _dimensions = new List<Dimension> {
                 new Dimension
                 {
                     Name = serviceCheckName,
                     Value = serviceCheckName
                 }
             };
-        }
+    }
 
-        /// <summary>
-        /// CloudWatchPublisher constructor
-        /// </summary>
-        /// <param name="region"></param>
-        /// <param name="awsAccessKeyId"></param>
-        /// <param name="awsSecretAccessKey"></param>
-        public CloudWatchPublisher(string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region)
-        {
-            _amazonCloudWatchClient = new AmazonCloudWatchClient(awsAccessKeyId, awsSecretAccessKey, region);
+    /// <summary>
+    /// CloudWatchPublisher constructor
+    /// </summary>
+    /// <param name="region"></param>
+    /// <param name="awsAccessKeyId"></param>
+    /// <param name="awsSecretAccessKey"></param>
+    public CloudWatchPublisher(string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region)
+    {
+        _amazonCloudWatchClient = new AmazonCloudWatchClient(awsAccessKeyId, awsSecretAccessKey, region);
 
-            var serviceCheckName = Assembly.GetEntryAssembly()?.GetName()?.Name ?? "undefined";
+        var serviceCheckName = Assembly.GetEntryAssembly()?.GetName()?.Name ?? "undefined";
 
-            _dimensions = new List<Dimension> {
+        _dimensions = new List<Dimension> {
                 new Dimension
                 {
                     Name = serviceCheckName,
                     Value = serviceCheckName
                 }
             };
-        }
+    }
 
-        /// <summary>
-        /// CloudWatchPublisher constructor
-        /// </summary>
-        /// <param name="serviceCheckName"></param>
-        /// <param name="region"></param>
-        /// <param name="awsAccessKeyId"></param>
-        /// <param name="awsSecretAccessKey"></param>
-        public CloudWatchPublisher(string serviceCheckName, string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region) : this(awsAccessKeyId, awsSecretAccessKey, region)
-        {
-            if (string.IsNullOrEmpty(serviceCheckName))
-                throw new ArgumentNullException(nameof(serviceCheckName));
+    /// <summary>
+    /// CloudWatchPublisher constructor
+    /// </summary>
+    /// <param name="serviceCheckName"></param>
+    /// <param name="region"></param>
+    /// <param name="awsAccessKeyId"></param>
+    /// <param name="awsSecretAccessKey"></param>
+    public CloudWatchPublisher(string serviceCheckName, string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region) : this(awsAccessKeyId, awsSecretAccessKey, region)
+    {
+        if (string.IsNullOrEmpty(serviceCheckName))
+            throw new ArgumentNullException(nameof(serviceCheckName));
 
-            _dimensions = new List<Dimension> {
+        _dimensions = new List<Dimension> {
                 new Dimension
                 {
                     Name = serviceCheckName,
                     Value = serviceCheckName
                 }
             };
-        }
+    }
 
-        /// <summary>
-        /// Publishes the HealthReport to AWS CloudWatch
-        /// </summary>
-        /// <param name="report"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
-        {
-            var putMetricDataRequest = BuildCloudWatchMetricDataRequest(report);
+    /// <summary>
+    /// Publishes the HealthReport to AWS CloudWatch
+    /// </summary>
+    /// <param name="report"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+    {
+        var putMetricDataRequest = BuildCloudWatchMetricDataRequest(report);
 
-            _amazonCloudWatchClient.PutMetricDataAsync(putMetricDataRequest, cancellationToken);
+        _amazonCloudWatchClient.PutMetricDataAsync(putMetricDataRequest, cancellationToken);
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
+    }
 
-        /// <summary>
-        /// Builds the CloudWatch MetricDataRequest
-        /// </summary>
-        /// <param name="report"></param>
-        /// <returns></returns>
-        private PutMetricDataRequest BuildCloudWatchMetricDataRequest(HealthReport report)
-        {
-            var utcNow = DateTime.UtcNow;
+    /// <summary>
+    /// Builds the CloudWatch MetricDataRequest
+    /// </summary>
+    /// <param name="report"></param>
+    /// <returns></returns>
+    private PutMetricDataRequest BuildCloudWatchMetricDataRequest(HealthReport report)
+    {
+        var utcNow = DateTime.UtcNow;
 
-            var metricDatas = new List<MetricDatum>
+        var metricDatas = new List<MetricDatum>
             {
                 new MetricDatum
                 {
@@ -129,44 +129,43 @@ namespace HealthChecks.Publisher.CloudWatch
                 }
             };
 
-            var entriesMetricDatas = report.Entries.Select(keyedEntry =>
+        var entriesMetricDatas = report.Entries.Select(keyedEntry =>
+        {
+            return new MetricDatum
             {
-                return new MetricDatum
-                {
-                    Dimensions = _dimensions,
-                    MetricName = keyedEntry.Key,
-                    StatisticValues = new StatisticSet(),
-                    TimestampUtc = utcNow,
-                    Unit = StandardUnit.Count,
-                    Value = (int)keyedEntry.Value.Status
-                };
-            });
-
-            if (entriesMetricDatas.Any())
-                metricDatas.AddRange(entriesMetricDatas);
-
-            return new PutMetricDataRequest
-            {
-                MetricData = metricDatas,
-                Namespace = "Xabaril/AspNetCoreDiagnosticsHealthChecks"
+                Dimensions = _dimensions,
+                MetricName = keyedEntry.Key,
+                StatisticValues = new StatisticSet(),
+                TimestampUtc = utcNow,
+                Unit = StandardUnit.Count,
+                Value = (int)keyedEntry.Value.Status
             };
-        }
+        });
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+        if (entriesMetricDatas.Any())
+            metricDatas.AddRange(entriesMetricDatas);
 
-        protected virtual void Dispose(bool disposing)
+        return new PutMetricDataRequest
         {
-            if (disposing)
-                _amazonCloudWatchClient?.Dispose();
-        }
+            MetricData = metricDatas,
+            Namespace = "Xabaril/AspNetCoreDiagnosticsHealthChecks"
+        };
+    }
 
-        ~CloudWatchPublisher()
-        {
-            Dispose(false);
-        }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+            _amazonCloudWatchClient?.Dispose();
+    }
+
+    ~CloudWatchPublisher()
+    {
+        Dispose(false);
     }
 }
--- src/HealthChecks.Publisher.CloudWatch/DependencyInjection/CloudWatchHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Publisher.CloudWatch/DependencyInjection/CloudWatchHealthCheckBuilderExtensions.cs
@@ -2,94 +2,93 @@ using Amazon;
 using HealthChecks.Publisher.CloudWatch;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class CloudWatchHealthCheckBuilderExtensions
 {
-    public static class CloudWatchHealthCheckBuilderExtensions
+    /// <summary>
+    /// Add a health check publisher for AWS CloudWatch.
+    /// </summary>
+    /// <remarks>
+    /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
+    /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
+    /// </remarks>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddCloudWatchPublisher(this IHealthChecksBuilder builder)
     {
-        /// <summary>
-        /// Add a health check publisher for AWS CloudWatch.
-        /// </summary>
-        /// <remarks>
-        /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
-        /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
-        /// </remarks>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <returns>The specified <paramref name="builder"/>.</returns>
-        public static IHealthChecksBuilder AddCloudWatchPublisher(this IHealthChecksBuilder builder)
-        {
-            builder.Services.AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>();
+        builder.Services.AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>();
 
-            return builder;
-        }
+        return builder;
+    }
 
-        /// <summary>
-        /// Add a health check publisher for AWS CloudWatch.
-        /// </summary>
-        /// <remarks>
-        /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
-        /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
-        /// </remarks>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="serviceCheckName">The <see cref="string"/>.</param>
-        /// <returns>The specified <paramref name="builder"/>.</returns>
-        public static IHealthChecksBuilder AddCloudWatchPublisher(
-            this IHealthChecksBuilder builder,
-            string serviceCheckName)
-        {
-            builder.Services
-               .AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>(sp => new CloudWatchPublisher(serviceCheckName));
+    /// <summary>
+    /// Add a health check publisher for AWS CloudWatch.
+    /// </summary>
+    /// <remarks>
+    /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
+    /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
+    /// </remarks>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="serviceCheckName">The <see cref="string"/>.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddCloudWatchPublisher(
+        this IHealthChecksBuilder builder,
+        string serviceCheckName)
+    {
+        builder.Services
+           .AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>(sp => new CloudWatchPublisher(serviceCheckName));
 
-            return builder;
-        }
+        return builder;
+    }
 
-        /// <summary>
-        /// Add a health check publisher for AWS CloudWatch.
-        /// </summary>
-        /// <remarks>
-        /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
-        /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
-        /// </remarks>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="awsAccessKeyId">The <see cref="string"/>.</param>
-        /// <param name="awsSecretAccessKey">The <see cref="string"/>.</param>
-        /// <param name="region">The <see cref="RegionEndpoint"/>.</param>
-        /// <returns>The specified <paramref name="builder"/>.</returns>
-        public static IHealthChecksBuilder AddCloudWatchPublisher(
-            this IHealthChecksBuilder builder,
-            string awsAccessKeyId,
-            string awsSecretAccessKey,
-            RegionEndpoint region)
-        {
-            builder.Services
-               .AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>(sp => new CloudWatchPublisher(awsAccessKeyId, awsSecretAccessKey, region));
+    /// <summary>
+    /// Add a health check publisher for AWS CloudWatch.
+    /// </summary>
+    /// <remarks>
+    /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
+    /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
+    /// </remarks>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="awsAccessKeyId">The <see cref="string"/>.</param>
+    /// <param name="awsSecretAccessKey">The <see cref="string"/>.</param>
+    /// <param name="region">The <see cref="RegionEndpoint"/>.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddCloudWatchPublisher(
+        this IHealthChecksBuilder builder,
+        string awsAccessKeyId,
+        string awsSecretAccessKey,
+        RegionEndpoint region)
+    {
+        builder.Services
+           .AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>(sp => new CloudWatchPublisher(awsAccessKeyId, awsSecretAccessKey, region));
 
-            return builder;
-        }
+        return builder;
+    }
 
-        /// <summary>
-        /// Add a health check publisher for AWS CloudWatch.
-        /// </summary>
-        /// <remarks>
-        /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
-        /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
-        /// </remarks>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="serviceCheckName">The <see cref="string"/>.</param>
-        /// <param name="awsAccessKeyId">The <see cref="string"/>.</param>
-        /// <param name="awsSecretAccessKey">The <see cref="string"/>.</param>
-        /// <param name="region">The <see cref="RegionEndpoint"/>.</param>
-        /// <returns>The specified <paramref name="builder"/>.</returns>
-        public static IHealthChecksBuilder AddCloudWatchPublisher(
-            this IHealthChecksBuilder builder,
-            string serviceCheckName,
-            string awsAccessKeyId,
-            string awsSecretAccessKey,
-            RegionEndpoint region)
-        {
-            builder.Services
-               .AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>(sp => new CloudWatchPublisher(serviceCheckName, awsAccessKeyId, awsSecretAccessKey, region));
+    /// <summary>
+    /// Add a health check publisher for AWS CloudWatch.
+    /// </summary>
+    /// <remarks>
+    /// For each <see cref="HealthReport"/> published a new event <c>AspNetCoreHealthCheck</c> is sent to AWS CloudWatch with two metrics <c>AspNetCoreHealthCheckStatus</c> and <c>AspNetCoreHealthCheckDuration</c>
+    /// indicating the health check status ( 1 Healthy 0 Unhealthy)  and the total time the health check took to execute on milliseconds./>
+    /// </remarks>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="serviceCheckName">The <see cref="string"/>.</param>
+    /// <param name="awsAccessKeyId">The <see cref="string"/>.</param>
+    /// <param name="awsSecretAccessKey">The <see cref="string"/>.</param>
+    /// <param name="region">The <see cref="RegionEndpoint"/>.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddCloudWatchPublisher(
+        this IHealthChecksBuilder builder,
+        string serviceCheckName,
+        string awsAccessKeyId,
+        string awsSecretAccessKey,
+        RegionEndpoint region)
+    {
+        builder.Services
+           .AddSingleton<IHealthCheckPublisher, CloudWatchPublisher>(sp => new CloudWatchPublisher(serviceCheckName, awsAccessKeyId, awsSecretAccessKey, region));
 
-            return builder;
-        }
+        return builder;
     }
 }

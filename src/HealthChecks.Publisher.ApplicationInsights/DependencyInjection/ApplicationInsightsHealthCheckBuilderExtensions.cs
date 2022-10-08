41150686ee35fb955faaea485d4e49a26904cb21
github@esslinger.dev <simon@esslinger.dev>
2022-10-08 13:32:44 +0200

    feat(AspNetcore.HealthChecks.Publisher.ApplicationInsights): add connection string support

--- src/HealthChecks.Publisher.ApplicationInsights/DependencyInjection/ApplicationInsightsHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Publisher.ApplicationInsights/DependencyInjection/ApplicationInsightsHealthCheckBuilderExtensions.cs
@@ -15,12 +15,14 @@ namespace Microsoft.Extensions.DependencyInjection
         /// indicating the health check status (1 - Healthy, 0 - Unhealthy) and the total time the health check took to execute in milliseconds.
         /// </remarks>
         /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="instrumentationKey">Specified Application Insights instrumentation key. Optional. If <c>null</c> TelemetryConfiguration.Active is used.</param>
+        /// <param name="connectionString">Specified Application Insights connection string. Optional. If <c>null</c> <see cref="TelemetryConfiguration"/> is resolved from DI container.</param>
+        /// <param name="instrumentationKey">Specified Application Insights instrumentation key. Optional. If <c>null</c> <see cref="TelemetryConfiguration"/> is resolved from DI container.</param>
         /// <param name="saveDetailedReport">Specifies if save an Application Insights event for each HealthCheck or just save one event with the global status for all the HealthChecks. Optional</param>
         /// <param name="excludeHealthyReports">Specifies if save an Application Insights event only for reports indicating an unhealthy status</param>
         /// <returns>The specified <paramref name="builder"/>.</returns>
         public static IHealthChecksBuilder AddApplicationInsightsPublisher(
             this IHealthChecksBuilder builder,
+            string? connectionString = default,
             string? instrumentationKey = default,
             bool saveDetailedReport = false,
             bool excludeHealthyReports = false)
@@ -29,7 +31,7 @@ namespace Microsoft.Extensions.DependencyInjection
                .AddSingleton<IHealthCheckPublisher>(sp =>
                {
                    var telemetryConfigurationOptions = sp.GetService<IOptions<TelemetryConfiguration>>();
-                   return new ApplicationInsightsPublisher(telemetryConfigurationOptions, instrumentationKey, saveDetailedReport, excludeHealthyReports);
+                   return new ApplicationInsightsPublisher(telemetryConfigurationOptions, connectionString, instrumentationKey, saveDetailedReport, excludeHealthyReports);
                });
 
             return builder;

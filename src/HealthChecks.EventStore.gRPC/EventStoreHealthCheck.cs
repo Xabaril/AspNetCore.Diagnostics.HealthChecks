4106e985c98c770d232130b48f8042cd216e35ca
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-23 22:59:03 +0200

    Finished the health check

---
+++ src/HealthChecks.EventStore.gRPC/EventStoreHealthCheck.cs
@@ -0,0 +1,31 @@
+using EventStore.Client;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthChecks.EventStore.gRPC
+{
+    public class EventStoreHealthCheck : IHealthCheck
+    {
+        private readonly string _connectionString;
+
+        public EventStoreHealthCheck(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await using var client = new EventStoreClient(EventStoreClientSettings.Create(_connectionString));
+
+                using var subscription = await client.SubscribeToAllAsync(FromAll.Start, (subscription, @event, token) => Task.CompletedTask, cancellationToken: cancellationToken);
+
+                return new HealthCheckResult(HealthStatus.Healthy);
+            }
+            catch (Exception exception)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, exception: exception);
+            }
+        }
+    }
+}

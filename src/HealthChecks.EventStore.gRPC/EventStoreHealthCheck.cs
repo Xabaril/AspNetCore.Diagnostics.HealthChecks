5c728136415818fb3e5a0d29f2acf0100b9bff9a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-24 19:55:15 +0200

    added xml summary to `EventStoreHealthCheck.CheckHealthAsync`

--- src/HealthChecks.EventStore.gRPC/EventStoreHealthCheck.cs
+++ src/HealthChecks.EventStore.gRPC/EventStoreHealthCheck.cs
@@ -12,12 +12,17 @@ public class EventStoreHealthCheck : IHealthCheck
         _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
     }
 
+    /// <summary>
+    /// Checks whether a gRPC connection can be made to <see cref="EventStore"/>, using the supplied <c>connectionString</c>
+    /// </summary>
+    /// <param name="context">Health check context. Provides the <see cref="HealthCheckRegistration"/>, which contains information regarding the registration of the <see cref="EventStoreHealthCheck"/>.</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be cancelled.</param>
+    /// <returns><c>Healthy</c> if a connection can be made, otherwise <c>Unhealthy</c></returns>
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
         try
         {
             await using var client = new EventStoreClient(EventStoreClientSettings.Create(_connectionString));
-
             using var subscription = await client.SubscribeToAllAsync(FromAll.End,
                 eventAppeared: (_, _, _) => Task.CompletedTask,
                 cancellationToken: cancellationToken);

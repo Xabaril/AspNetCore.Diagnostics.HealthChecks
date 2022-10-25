f44b8ad546eb1a309e30b6d338ca70994fa2a563
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-25 10:59:05 +0200

    moved general description summary to class

--- src/HealthChecks.EventStore.gRPC/EventStoreHealthCheck.cs
+++ src/HealthChecks.EventStore.gRPC/EventStoreHealthCheck.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace HealthChecks.EventStore.gRPC;
 
+/// <summary>
+/// Checks whether a gRPC connection can be made to <see cref="EventStore"/>, using the supplied <c>connectionString</c>
+/// </summary>
 public class EventStoreHealthCheck : IHealthCheck
 {
     private readonly EventStoreClient _client;
@@ -14,9 +17,7 @@ public class EventStoreHealthCheck : IHealthCheck
         _client = new EventStoreClient(EventStoreClientSettings.Create(connectionString));
     }
 
-    /// <summary>
-    /// Checks whether a gRPC connection can be made to <see cref="EventStore"/>, using the supplied <c>connectionString</c>
-    /// </summary>
+    /// <inheritdoc/>
     /// <param name="context">Health check context. Provides the <see cref="HealthCheckRegistration"/>, which contains information regarding the registration of the <see cref="EventStoreHealthCheck"/>.</param>
     /// <param name="cancellationToken">Propagates notification that operations should be cancelled.</param>
     /// <returns><c>Healthy</c> if a connection can be made, otherwise <c>Unhealthy</c></returns>

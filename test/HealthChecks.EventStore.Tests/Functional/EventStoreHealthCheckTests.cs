9668e529a4cf55a4e5ca6754cc0281da9206c7d5
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-08 22:19:33 +0100

    repurposed old test to test the ieventstoreconnection approach
    Uri format does not work with this package anymore anyway, it hasn't since at least version 5.0.x

--- test/HealthChecks.EventStore.Tests/Functional/EventStoreHealthCheckTests.cs
+++ test/HealthChecks.EventStore.Tests/Functional/EventStoreHealthCheckTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using EventStore.ClientAPI;
 using HealthChecks.UI.Client;
 
 namespace HealthChecks.EventStore.Tests.Functional
@@ -6,13 +7,18 @@ namespace HealthChecks.EventStore.Tests.Functional
     public class eventstore_healthcheck_should
     {
         [Fact]
-        public async Task be_healthy_if_eventstore_is_available_with_uri_format()
+        public async Task be_healthy_if_eventstore_is_available_using_eventstoreconnection()
         {
+            var connectionSettingsBuilder = ConnectionSettings
+                .Create()
+                .SetCompatibilityMode("auto")
+                .DisableTls();
+
             var webHostBuilder = new WebHostBuilder()
                 .ConfigureServices(services =>
                 {
                     services.AddHealthChecks()
-                        .AddEventStore("ConnectTo=tcp://localhost:1113; UseSslConnection=false", tags: new string[] { "eventstore" });
+                        .AddEventStore(EventStoreConnection.Create(connectionSettingsBuilder, new IPEndPoint(IPAddress.Loopback, 1113)), tags: new string[] { "eventstore" });
                 })
                 .Configure(app =>
                 {

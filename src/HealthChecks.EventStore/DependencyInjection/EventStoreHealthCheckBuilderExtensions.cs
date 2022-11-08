66471692441a6963281b3905c6247b9487380b0e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-08 22:18:25 +0100

    added registration method

--- src/HealthChecks.EventStore/DependencyInjection/EventStoreHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.EventStore/DependencyInjection/EventStoreHealthCheckBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using EventStore.ClientAPI;
 using HealthChecks.EventStore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -38,6 +39,35 @@ namespace Microsoft.Extensions.DependencyInjection
             return builder.Add(new HealthCheckRegistration(
                 name ?? NAME,
                 sp => new EventStoreHealthCheck(eventStoreConnection, login, password),
+                failureStatus,
+                tags,
+                timeout));
+        }
+
+        /// <summary>
+        /// Add a health check for EventStore services.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="eventStoreConnection"> The EventStore Connection to be used. <para>This should be *not* be running, nor come from dependency injection.</para></param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'eventstore' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+        /// <returns>The specified <paramref name="builder"/>.</returns>
+        public static IHealthChecksBuilder AddEventStore(
+            this IHealthChecksBuilder builder,
+            IEventStoreConnection eventStoreConnection,
+            string? name = default,
+            HealthStatus? failureStatus = default,
+            IEnumerable<string>? tags = default,
+            TimeSpan? timeout = default)
+        {
+            return builder.Add(new HealthCheckRegistration(
+                name ?? NAME,
+                sp => new EventStoreHealthCheck(eventStoreConnection),
                 failureStatus,
                 tags,
                 timeout));

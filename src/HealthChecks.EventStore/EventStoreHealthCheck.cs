580ff8cfd9ada7052abd6683f46fb95315435174
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-10 23:20:48 +0100

    Remade EventStoreHealthCheck as a singleton
    The connection is now long lived, and will be disposed when the healthcheck does.
    
    Added support for passing in running IEventStoreConnections
    
    Added Guard clauses
    
    Made the tcs reusable
    
    Removed fields we no longer need, since the connection is built in the constructor

--- src/HealthChecks.EventStore/EventStoreHealthCheck.cs
+++ src/HealthChecks.EventStore/EventStoreHealthCheck.cs
@@ -4,84 +4,74 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace HealthChecks.EventStore
 {
-    public class EventStoreHealthCheck : IHealthCheck
+    public class EventStoreHealthCheck : IHealthCheck, IDisposable
     {
         private const string CONNECTION_NAME = "AspNetCore HealthCheck Connection";
         private const int ELAPSED_DELAY_MILLISECONDS = 500;
         private const int RECONNECTION_LIMIT = 1;
 
-        private readonly IEventStoreConnection? _eventStoreConnection;
-        private readonly string? _eventStoreConnectionString;
-        private readonly string? _login;
-        private readonly string? _password;
+        private readonly IEventStoreConnection _eventStoreConnection;
 
+        private readonly TaskCompletionSource<HealthCheckResult> _tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
         public EventStoreHealthCheck(string eventStoreConnection, string? login, string? password)
         {
-            _eventStoreConnectionString = Guard.ThrowIfNull(eventStoreConnection);
-            _login = login;
-            _password = password;
+            Guard.ThrowIfNull(eventStoreConnection, throwOnEmptyString: true);
+            _eventStoreConnection ??= CreateEventStoreConnection(eventStoreConnection, login, password);
         }
 
         public EventStoreHealthCheck(IEventStoreConnection eventStoreConnection)
         {
-            _eventStoreConnection = eventStoreConnection ?? throw new ArgumentNullException(nameof(eventStoreConnection));
+            _eventStoreConnection = Guard.ThrowIfNull(eventStoreConnection);
         }
 
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             try
             {
-                var connectionSettingsBuilder = ConnectionSettings.Create()
-                    .LimitReconnectionsTo(RECONNECTION_LIMIT)
-                    .SetReconnectionDelayTo(TimeSpan.FromMilliseconds(ELAPSED_DELAY_MILLISECONDS))
-                    .SetCompatibilityMode("auto");
+                //connected
+                _eventStoreConnection.Connected += (s, e) => _tcs.TrySetResult(HealthCheckResult.Healthy());
 
-                if (!string.IsNullOrEmpty(_login) && !string.IsNullOrEmpty(_password))
-                {
-                    connectionSettingsBuilder.SetDefaultUserCredentials(new UserCredentials(_login, _password));
-                }
-
-                using (var connection = _eventStoreConnection ??
-                                        EventStoreConnection.Create(
-                                            _eventStoreConnectionString,
-                                            connectionSettingsBuilder,
-                                            CONNECTION_NAME))
-                {
-
-                    var tcs = new TaskCompletionSource<HealthCheckResult>(
-                        TaskCreationOptions.RunContinuationsAsynchronously);
-
-                    //connected
-                    connection.Connected += (s, e) => tcs.TrySetResult(HealthCheckResult.Healthy());
-
-                    //connection closed after configured amount of failed reconnections
-                    connection.Closed += (s, e) =>
+                //connection closed after configured amount of failed reconnections
+                _eventStoreConnection.Closed += (s, e) =>
                     {
-                        tcs.TrySetResult(new HealthCheckResult(
+                        _tcs.TrySetResult(new HealthCheckResult(
                             status: context.Registration.FailureStatus,
                             description: e.Reason));
                     };
 
-                    //connection error
-                    connection.ErrorOccurred += (s, e) =>
+                //connection error
+                _eventStoreConnection.ErrorOccurred += (s, e) =>
                     {
-                        tcs.TrySetResult(new HealthCheckResult(
+                        _tcs.TrySetResult(new HealthCheckResult(
                             status: context.Registration.FailureStatus,
                             exception: e.Exception));
                     };
 
-                    using (cancellationToken.Register(() => connection.Close()))
+                using (cancellationToken.Register(() => _eventStoreConnection.Close()))
+                {
+                    try
                     {
                         //completes after tcp connection init, but before successful connection and login
-                        await connection.ConnectAsync();
+                        await _eventStoreConnection.ConnectAsync();
                     }
-
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    using (cancellationToken.Register(() => tcs.TrySetCanceled()))
+                    catch (InvalidOperationException exception)
                     {
-                        return await tcs.Task;
+                        if (ConnectionIsAlreadyRunning(exception))
+                        {
+                            _tcs.TrySetResult(HealthCheckResult.Healthy());
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
+                }
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                using (cancellationToken.Register(() => _tcs.TrySetCanceled()))
+                {
+                    return await _tcs.Task;
                 }
             }
             catch (Exception ex)
@@ -89,5 +79,28 @@ namespace HealthChecks.EventStore
                 return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
             }
         }
+
+        private bool ConnectionIsAlreadyRunning(InvalidOperationException exception)
+            => exception.Message == $"EventStoreConnection '{_eventStoreConnection!.ConnectionName}' is already active.";
+
+        private static IEventStoreConnection CreateEventStoreConnection(string eventStoreConnectionString, string? login, string? password)
+        {
+            var connectionSettingsBuilder = ConnectionSettings.Create()
+                .LimitReconnectionsTo(RECONNECTION_LIMIT)
+                .SetReconnectionDelayTo(TimeSpan.FromMilliseconds(ELAPSED_DELAY_MILLISECONDS))
+                .SetCompatibilityMode("auto");
+
+            if (!string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(password))
+            {
+                connectionSettingsBuilder.SetDefaultUserCredentials(new UserCredentials(login, password));
+            }
+
+            return EventStoreConnection.Create(
+                eventStoreConnectionString,
+                connectionSettingsBuilder,
+                CONNECTION_NAME);
+        }
+
+        public void Dispose() => _eventStoreConnection.Close();
     }
 }

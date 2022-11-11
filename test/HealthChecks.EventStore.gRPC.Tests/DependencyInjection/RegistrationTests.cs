734688ecfcbaff56d96b4b3725bf09048ba0b695
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-11 23:57:08 +0100

    Registration methods for EventStore TCP+gRPC passing in the ServiceProvider (#1538)

--- test/HealthChecks.EventStore.gRPC.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.EventStore.gRPC.Tests/DependencyInjection/RegistrationTests.cs
@@ -20,6 +20,23 @@ public class eventstore_registration_should
     }
 
     [Fact]
+    public void add_health_check_when_properly_configured_using_service_provider_overload()
+    {
+        var services = new ServiceCollection();
+        services.AddHealthChecks()
+            .AddEventStore(sp => "esdb://localhost:2113?tls=false");
+
+        using var serviceProvider = services.BuildServiceProvider();
+        var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
+
+        var registration = options.Value.Registrations.First();
+        var check = registration.Factory(serviceProvider);
+
+        registration.Name.ShouldBe("eventstore");
+        check.ShouldBeOfType<EventStoreHealthCheck>();
+    }
+
+    [Fact]
     public void add_named_health_check_when_properly_configured()
     {
         var services = new ServiceCollection();

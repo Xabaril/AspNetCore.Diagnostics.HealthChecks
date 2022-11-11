3de0dfe63640dac68dbbfa7bb82b90895036aa37
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-11 19:41:23 +0100

    remembered to unsubscribe from event handlers

--- src/HealthChecks.EventStore/EventStoreHealthCheck.cs
+++ src/HealthChecks.EventStore/EventStoreHealthCheck.cs
@@ -13,39 +13,26 @@ namespace HealthChecks.EventStore
         private readonly IEventStoreConnection _eventStoreConnection;
 
         private readonly TaskCompletionSource<HealthCheckResult> _tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
         public EventStoreHealthCheck(string eventStoreConnection, string? login, string? password)
         {
             Guard.ThrowIfNull(eventStoreConnection, throwOnEmptyString: true);
             _eventStoreConnection ??= CreateEventStoreConnection(eventStoreConnection, login, password);
+            _eventStoreConnection.Connected += OnConnectionConnected();
         }
 
         public EventStoreHealthCheck(IEventStoreConnection eventStoreConnection)
         {
             _eventStoreConnection = Guard.ThrowIfNull(eventStoreConnection);
+            _eventStoreConnection.Connected += OnConnectionConnected();
         }
 
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             try
             {
-                //connected
-                _eventStoreConnection.Connected += (s, e) => _tcs.TrySetResult(HealthCheckResult.Healthy());
-
-                //connection closed after configured amount of failed reconnections
-                _eventStoreConnection.Closed += (s, e) =>
-                    {
-                        _tcs.TrySetResult(new HealthCheckResult(
-                            status: context.Registration.FailureStatus,
-                            description: e.Reason));
-                    };
-
-                //connection error
-                _eventStoreConnection.ErrorOccurred += (s, e) =>
-                    {
-                        _tcs.TrySetResult(new HealthCheckResult(
-                            status: context.Registration.FailureStatus,
-                            exception: e.Exception));
-                    };
+                _eventStoreConnection.Closed += OnConnectionClosed(context);
+                _eventStoreConnection.ErrorOccurred += OnConnectionErrorOccurred(context);
 
                 using (cancellationToken.Register(() => _eventStoreConnection.Close()))
                 {
@@ -71,14 +58,52 @@ namespace HealthChecks.EventStore
 
                 using (cancellationToken.Register(() => _tcs.TrySetCanceled()))
                 {
-                    return await _tcs.Task;
+                    var healthCheckResult = await _tcs.Task;
+                    _eventStoreConnection.Closed -= OnConnectionClosed(context);
+                    _eventStoreConnection.ErrorOccurred -= OnConnectionErrorOccurred(context);
+                    return healthCheckResult;
                 }
             }
             catch (Exception ex)
             {
+                _eventStoreConnection.Closed -= OnConnectionClosed(context);
+                _eventStoreConnection.ErrorOccurred -= OnConnectionErrorOccurred(context);
                 return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
             }
         }
+
+        /// <summary>
+        /// <see cref="IEventStoreConnection.ErrorOccurred"/> is invoked when an error is thrown on the <see cref="_eventStoreConnection"/>.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private EventHandler<ClientErrorEventArgs> OnConnectionErrorOccurred(HealthCheckContext context) =>
+            (s, e) =>
+            {
+                _tcs.TrySetResult(new HealthCheckResult(
+                    status: context.Registration.FailureStatus,
+                    exception: e.Exception));
+            };
+
+        /// <summary>
+        /// <see cref="IEventStoreConnection.Connected"/> is invoked when the <see cref="_eventStoreConnection"/> has established a connection to EventStore.
+        /// </summary>
+        /// <returns></returns>
+        private EventHandler<ClientConnectionEventArgs> OnConnectionConnected() =>
+            (s, e) => _tcs.TrySetResult(HealthCheckResult.Healthy());
+
+        /// <summary>
+        /// <see cref="IEventStoreConnection.Closed"/> is invoked after reconnection has failed the configured amount of times.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private EventHandler<ClientClosedEventArgs> OnConnectionClosed(HealthCheckContext context) =>
+            (s, e) =>
+            {
+                _tcs.TrySetResult(new HealthCheckResult(
+                    status: context.Registration.FailureStatus,
+                    description: e.Reason));
+            };
 
         private bool ConnectionIsAlreadyRunning(InvalidOperationException exception)
             => exception.Message == $"EventStoreConnection '{_eventStoreConnection!.ConnectionName}' is already active.";

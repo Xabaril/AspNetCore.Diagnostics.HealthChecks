a41905db07f68f8bf89c07df2ad94b96a98f500c
Ivan Maximov <sungam3r@yandex.ru>
2022-12-18 02:14:35 +0300

    Prepare projects for v7 packages

--- test/TestLogger.cs
+++ test/TestLogger.cs
@@ -3,15 +3,7 @@ using Microsoft.Extensions.Logging;
 public sealed class TestLogger : ILogger
 {
     public List<(DateTime, string)> EventLog { get; } = new();
-    public IDisposable
-#if NET7_0_OR_GREATER
-        ?
-#endif
-        BeginScope<TState>(TState state)
-#if NET7_0_OR_GREATER
-        where TState : notnull
-#endif
-        => default!;
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default!;
     public bool IsEnabled(LogLevel logLevel) => true;
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
         => EventLog.Add(new(DateTime.Now, formatter(state, exception)));

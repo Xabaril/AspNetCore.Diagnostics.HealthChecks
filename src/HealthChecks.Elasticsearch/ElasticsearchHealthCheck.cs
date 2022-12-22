4b239dfa0571ec800d3a36afb9e5dea176a6e29c
Ivan Maximov <sungam3r@yandex.ru>
2022-12-22 18:12:03 +0300

    Update src/HealthChecks.Elasticsearch/ElasticsearchHealthCheck.cs

--- src/HealthChecks.Elasticsearch/ElasticsearchHealthCheck.cs
+++ src/HealthChecks.Elasticsearch/ElasticsearchHealthCheck.cs
@@ -17,7 +17,6 @@ namespace HealthChecks.Elasticsearch
         }
 
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
-
         {
             try
             {

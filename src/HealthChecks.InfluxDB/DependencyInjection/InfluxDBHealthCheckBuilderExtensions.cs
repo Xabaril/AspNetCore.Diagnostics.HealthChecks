9723e518c03b3fe011444b24ae43be02119402d8
Ivan Maximov <sungam3r@yandex.ru>
2022-12-23 18:09:26 +0300

    Update src/HealthChecks.InfluxDB/DependencyInjection/InfluxDBHealthCheckBuilderExtensions.cs

--- src/HealthChecks.InfluxDB/DependencyInjection/InfluxDBHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.InfluxDB/DependencyInjection/InfluxDBHealthCheckBuilderExtensions.cs
@@ -45,7 +45,7 @@ public static class InfluxDBHealthCheckBuilderExtensions
     /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
     /// </param>
     /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
-    /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
     /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
     public static IHealthChecksBuilder AddInfluxDB(this IHealthChecksBuilder builder, Uri uri, string? name = default, HealthStatus? failureStatus = default, IEnumerable<string>? tags = default, TimeSpan? timeout = default)
     {

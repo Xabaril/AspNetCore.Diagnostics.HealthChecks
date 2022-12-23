274f16330e971daa124b26971c93b26931cc581e
MysticBoy <mysticboy@live.com>
2022-12-23 23:10:57 +0800

    file-scoped

--- test/HealthChecks.InfluxDB.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.InfluxDB.Tests/DependencyInjection/RegistrationTests.cs
@@ -1,22 +1,21 @@
-namespace HealthChecks.InfluxDB.Tests.DependencyInjection
+namespace HealthChecks.InfluxDB.Tests.DependencyInjection;
+
+public class influxdb_registration_should
 {
-    public class influxdb_registration_should
+    [Fact]
+    public void add_health_check_when_properly_configured()
     {
-        [Fact]
-        public void add_health_check_when_properly_configured()
-        {
-            var services = new ServiceCollection();
-            services
-                .AddHealthChecks()
-                .AddInfluxDB("http://localhost:8086/?org=influxdata&bucket=dummy&latest=-72h", "ci_user", "password", "influxdb");
+        var services = new ServiceCollection();
+        services
+            .AddHealthChecks()
+            .AddInfluxDB("http://localhost:8086/?org=influxdata&bucket=dummy&latest=-72h", "ci_user", "password", "influxdb");
 
-            using var serviceProvider = services.BuildServiceProvider();
-            var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
-            var registration = options?.Value.Registrations.First();
-            var check = registration?.Factory(serviceProvider);
+        using var serviceProvider = services.BuildServiceProvider();
+        var options = serviceProvider.GetService<IOptions<HealthCheckServiceOptions>>();
+        var registration = options?.Value.Registrations.First();
+        var check = registration?.Factory(serviceProvider);
 
-            registration?.Name.ShouldBe("influxdb");
-            check.ShouldBeOfType<InfluxDBHealthCheck>();
-        }
+        registration?.Name.ShouldBe("influxdb");
+        check.ShouldBeOfType<InfluxDBHealthCheck>();
     }
 }

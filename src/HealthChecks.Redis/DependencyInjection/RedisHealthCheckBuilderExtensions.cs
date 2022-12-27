502c0f1035e608e5cdb5112c9ecf8b56d12fbbf0
Ivan Maximov <sungam3r@yandex.ru>
2022-12-28 01:52:32 +0300

    File-scoped namespaces for RabbitMQ, RavenDb, Redis (#1605)

--- src/HealthChecks.Redis/DependencyInjection/RedisHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Redis/DependencyInjection/RedisHealthCheckBuilderExtensions.cs
@@ -1,68 +1,67 @@
 using HealthChecks.Redis;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods to configure <see cref="RedisHealthCheck"/>.
+/// </summary>
+public static class RedisHealthCheckBuilderExtensions
 {
+    private const string NAME = "redis";
+
     /// <summary>
-    /// Extension methods to configure <see cref="RedisHealthCheck"/>.
+    /// Add a health check for Redis services.
     /// </summary>
-    public static class RedisHealthCheckBuilderExtensions
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="redisConnectionString">The Redis connection string to be used.</param>
+    /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'redis' will be used for the name.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+    /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddRedis(
+        this IHealthChecksBuilder builder,
+        string redisConnectionString,
+        string? name = default,
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default)
     {
-        private const string NAME = "redis";
+        return builder.AddRedis(_ => redisConnectionString, name, failureStatus, tags, timeout);
+    }
 
-        /// <summary>
-        /// Add a health check for Redis services.
-        /// </summary>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="redisConnectionString">The Redis connection string to be used.</param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'redis' will be used for the name.</param>
-        /// <param name="failureStatus">
-        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
-        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
-        /// </param>
-        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
-        /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
-        /// <returns>The specified <paramref name="builder"/>.</returns>
-        public static IHealthChecksBuilder AddRedis(
-            this IHealthChecksBuilder builder,
-            string redisConnectionString,
-            string? name = default,
-            HealthStatus? failureStatus = default,
-            IEnumerable<string>? tags = default,
-            TimeSpan? timeout = default)
-        {
-            return builder.AddRedis(_ => redisConnectionString, name, failureStatus, tags, timeout);
-        }
+    /// <summary>
+    /// Add a health check for Redis services.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="connectionStringFactory">A factory to build the Redis connection string to use.</param>
+    /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'redis' will be used for the name.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+    /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddRedis(
+        this IHealthChecksBuilder builder,
+        Func<IServiceProvider, string> connectionStringFactory,
+        string? name = default,
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default)
+    {
+        Guard.ThrowIfNull(connectionStringFactory);
 
-        /// <summary>
-        /// Add a health check for Redis services.
-        /// </summary>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="connectionStringFactory">A factory to build the Redis connection string to use.</param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'redis' will be used for the name.</param>
-        /// <param name="failureStatus">
-        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
-        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
-        /// </param>
-        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
-        /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
-        /// <returns>The specified <paramref name="builder"/>.</returns>
-        public static IHealthChecksBuilder AddRedis(
-            this IHealthChecksBuilder builder,
-            Func<IServiceProvider, string> connectionStringFactory,
-            string? name = default,
-            HealthStatus? failureStatus = default,
-            IEnumerable<string>? tags = default,
-            TimeSpan? timeout = default)
-        {
-            Guard.ThrowIfNull(connectionStringFactory);
-
-            return builder.Add(new HealthCheckRegistration(
-               name ?? NAME,
-               sp => new RedisHealthCheck(connectionStringFactory(sp)),
-               failureStatus,
-               tags,
-               timeout));
-        }
+        return builder.Add(new HealthCheckRegistration(
+           name ?? NAME,
+           sp => new RedisHealthCheck(connectionStringFactory(sp)),
+           failureStatus,
+           tags,
+           timeout));
     }
 }

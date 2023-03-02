53013a1b6441bd9892669569503b16b9e38ea6fd
Bilal Durrani <bilal.durrani@xero.com>
2023-03-01 21:26:54 -0500

    implement list request object

--- src/HealthChecks.Aws.S3/S3HealthCheck.cs
+++ src/HealthChecks.Aws.S3/S3HealthCheck.cs
@@ -1,5 +1,6 @@
 using Amazon.Runtime;
 using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace HealthChecks.Aws.S3;
@@ -41,7 +42,12 @@ public class S3HealthCheck : IHealthCheck
 
             using (client)
             {
-                var response = await client.ListObjectsAsync(_bucketOptions.BucketName, cancellationToken).ConfigureAwait(false);
+                var listRequest = new ListObjectsRequest
+                {
+                    BucketName = _bucketOptions.BucketName,
+                    MaxKeys = _bucketOptions.MaxKeys
+                };
+                var response = await client.ListObjectsAsync(listRequest, cancellationToken).ConfigureAwait(false);
 
                 if (_bucketOptions.CustomResponseCheck != null)
                 {

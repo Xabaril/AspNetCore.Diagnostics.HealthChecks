b40a2accac02bff286c2b24b19213680a71f9f7c
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2023-07-03 11:07:49 +0200

    Add parameters description;

--- src/HealthChecks.AzureSearch/AzureSearchOptions.cs
+++ src/HealthChecks.AzureSearch/AzureSearchOptions.cs
@@ -5,9 +5,18 @@ namespace HealthChecks.AzureSearch;
 /// </summary>
 public class AzureSearchOptions
 {
+    /// <summary>
+    /// The URI endpoint of the Azure Search
+    /// </summary>
     public string Endpoint { get; set; } = null!;
 
+    /// <summary>
+    /// The name of the Search index
+    /// </summary>
     public string IndexName { get; set; } = null!;
 
+    /// <summary>
+    /// The API credential used to authenticate against the Search service
+    /// </summary>
     public string AuthKey { get; set; } = null!;
 }

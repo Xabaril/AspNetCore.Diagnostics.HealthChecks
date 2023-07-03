f7ac86d263787b1d89945604c934ad6489aff80a
Ivan Maximov <sungam3r@yandex.ru>
2023-07-03 12:15:51 +0300

    Apply suggestions from code review

--- src/HealthChecks.AzureSearch/AzureSearchOptions.cs
+++ src/HealthChecks.AzureSearch/AzureSearchOptions.cs
@@ -6,17 +6,17 @@ namespace HealthChecks.AzureSearch;
 public class AzureSearchOptions
 {
     /// <summary>
-    /// The URI endpoint of the Azure Search
+    /// The URI endpoint of the Azure Search.
     /// </summary>
     public string Endpoint { get; set; } = null!;
 
     /// <summary>
-    /// The name of the Search index
+    /// The name of the Search index.
     /// </summary>
     public string IndexName { get; set; } = null!;
 
     /// <summary>
-    /// The API credential used to authenticate against the Search service
+    /// The API credential used to authenticate against the Search service.
     /// </summary>
     public string AuthKey { get; set; } = null!;
 }

3dc88517ad7ef5a400cf0b807f3bd5d3d262e9c4
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2023-07-04 08:13:23 +0200

    Revert "Fix tests"
    This reverts commit 18c9fb7b3b5af4f563924c14060f79164802ae82.

---
+++ test/HealthChecks.Elasticsearch.Tests/Functional/ElasticsearchAuthenticationTests.cs
@@ -0,0 +1,87 @@
+using System.Net;
+using Elasticsearch.Net;
+using HealthChecks.Elasticsearch.Tests.Fixtures;
+
+namespace HealthChecks.Elasticsearch.Tests.Functional;
+
+public class ElasticsearchAuthenticationTests : IClassFixture<ElasticContainerFixture>
+{
+    private readonly ElasticContainerFixture _fixture;
+
+    public ElasticsearchAuthenticationTests(ElasticContainerFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task be_healthy_if_elasticsearch_is_using_valid_api_key()
+    {
+        var connectionString = @"https://localhost:9200";
+
+        var webHostBuilder = new WebHostBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddHealthChecks()
+                    .AddElasticsearch(options =>
+                    {
+                        options.UseServer(connectionString);
+                        options.UseApiKey(new ApiKeyAuthenticationCredentials(_fixture.ApiKey));
+                        options.UseCertificateValidationCallback(delegate
+                        {
+                            return true;
+                        });
+                        options.RequestTimeout = TimeSpan.FromSeconds(30);
+                    }, tags: new[] { "elasticsearch" });
+            })
+            .Configure(app =>
+            {
+                app.UseHealthChecks("/health",
+                    new HealthCheckOptions { Predicate = r => r.Tags.Contains("elasticsearch") });
+            });
+
+        using var server = new TestServer(webHostBuilder);
+
+        var response = await server.CreateRequest($"/health")
+            .GetAsync()
+            .ConfigureAwait(false);
+
+        response.StatusCode
+            .ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task be_healthy_if_elasticsearch_is_using_valid_user_and_password()
+    {
+        var connectionString = @"https://localhost:9200";
+
+        var webHostBuilder = new WebHostBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddHealthChecks()
+                    .AddElasticsearch(options =>
+                    {
+                        options.UseServer(connectionString);
+                        options.UseBasicAuthentication("elastic", ElasticContainerFixture.ELASTIC_PASSWORD);
+                        options.UseCertificateValidationCallback(delegate
+                        {
+                            return true;
+                        });
+                        options.RequestTimeout = TimeSpan.FromSeconds(30);
+                    }, tags: new[] { "elasticsearch" });
+            })
+            .Configure(app =>
+            {
+                app.UseHealthChecks("/health",
+                    new HealthCheckOptions { Predicate = r => r.Tags.Contains("elasticsearch") });
+            });
+
+        using var server = new TestServer(webHostBuilder);
+
+        var response = await server.CreateRequest($"/health")
+            .GetAsync()
+            .ConfigureAwait(false);
+
+        response.StatusCode
+            .ShouldBe(HttpStatusCode.OK);
+    }
+}

db477385581429cf2240ce0949d82dd01f9920fd
Ivan Maximov <sungam3r@yandex.ru>
2023-07-06 02:03:08 +0300

    NSJ -> STJ

--- test/HealthChecks.UI.Tests/Seedwork/HttpClientExtensions.cs
+++ test/HealthChecks.UI.Tests/Seedwork/HttpClientExtensions.cs
@@ -1,19 +1,16 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+using System.Text.Json;
 
-namespace HealthChecks.UI.Tests
+namespace HealthChecks.UI.Tests;
+
+public static class HttpClientExtensions
 {
-    public static class HttpClientExtensions
+    public static async Task<T> GetAsJson<T>(this HttpClient client, string url)
     {
-        public static async Task<T> GetAsJson<T>(this HttpClient client, string url)
+        var response = await client.GetAsync(url).ConfigureAwait(false);
+        string content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return JsonSerializer.Deserialize<T>(content, new JsonSerializerOptions
         {
-            var response = await client.GetAsync(url).ConfigureAwait(false);
-            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<T>(content, new JsonSerializerSettings
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                DateTimeZoneHandling = DateTimeZoneHandling.Local
-            })!;
-        }
+            PropertyNameCaseInsensitive = true,
+        })!;
     }
 }

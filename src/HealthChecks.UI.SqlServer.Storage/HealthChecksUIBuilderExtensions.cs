00d04adf3fffa7186c608897949a906d70df70a5
Ivan Maximov <sungam3r@yandex.ru>
2023-07-05 14:26:14 +0300

    Add possibility to pass custom DbContextOptionsBuilder (#1858)

--- src/HealthChecks.UI.SqlServer.Storage/HealthChecksUIBuilderExtensions.cs
+++ src/HealthChecks.UI.SqlServer.Storage/HealthChecksUIBuilderExtensions.cs
@@ -1,16 +1,25 @@
 using HealthChecks.UI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class HealthChecksUIBuilderExtensions
     {
-        public static HealthChecksUIBuilder AddSqlServerStorage(this HealthChecksUIBuilder builder, string connectionString, Action<DbContextOptionsBuilder>? configureOptions = null)
+        public static HealthChecksUIBuilder AddSqlServerStorage(
+            this HealthChecksUIBuilder builder,
+            string connectionString,
+            Action<DbContextOptionsBuilder>? configureOptions = null,
+            Action<SqlServerDbContextOptionsBuilder>? configureSqlServerOptions = null)
         {
-            builder.Services.AddDbContext<HealthChecksDb>(options =>
+            builder.Services.AddDbContext<HealthChecksDb>(optionsBuilder =>
             {
-                configureOptions?.Invoke(options);
-                options.UseSqlServer(connectionString, s => s.MigrationsAssembly("HealthChecks.UI.SqlServer.Storage"));
+                configureOptions?.Invoke(optionsBuilder);
+                optionsBuilder.UseSqlServer(connectionString, sqlServerOptionsBuilder =>
+                {
+                    sqlServerOptionsBuilder.MigrationsAssembly("HealthChecks.UI.SqlServer.Storage");
+                    configureSqlServerOptions?.Invoke(sqlServerOptionsBuilder);
+                });
             });
 
             return builder;

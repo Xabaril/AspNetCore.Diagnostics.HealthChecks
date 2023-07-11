9e39b05fe0c234395b0e11617c9a5abb01b3019a
Ivan Maximov <sungam3r@yandex.ru>
2023-07-11 13:57:11 +0300

    revert

--- src/HealthChecks.UI.InMemory.Storage/HealthChecksUIBuilderExtensions.cs
+++ src/HealthChecks.UI.InMemory.Storage/HealthChecksUIBuilderExtensions.cs
@@ -6,12 +6,8 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class HealthChecksUIBuilderExtensions
     {
         public static HealthChecksUIBuilder AddInMemoryStorage(this HealthChecksUIBuilder builder, Action<DbContextOptionsBuilder>? configureOptions = null, string databaseName = "HealthChecksUI")
-            => builder.AddInMemoryStorage<HealthChecksDb>(configureOptions, databaseName);
-
-        public static HealthChecksUIBuilder AddInMemoryStorage<THealthChecksDb>(this HealthChecksUIBuilder builder, Action<DbContextOptionsBuilder>? configureOptions = null, string databaseName = "HealthChecksUI")
-            where THealthChecksDb : HealthChecksDb
         {
-            builder.Services.AddDbContext<HealthChecksDb, THealthChecksDb>(options =>
+            builder.Services.AddDbContext<HealthChecksDb>(options =>
             {
                 configureOptions?.Invoke(options);
                 options.UseInMemoryDatabase(databaseName);

ece2971a9ba81f452868998556f1dbd310062b36
Ivan Maximov <sungam3r@yandex.ru>
2023-07-11 13:49:14 +0300

    internal

--- src/HealthChecks.UI/Configuration/Settings.cs
+++ src/HealthChecks.UI/Configuration/Settings.cs
@@ -6,8 +6,7 @@ namespace HealthChecks.UI.Configuration
 {
     public class Settings
     {
-        public Action<List<HealthCheckExecution>>? ConfigureUIApiEndpointResult { get; set; }
-
+        internal Action<List<HealthCheckExecution>>? ConfigureUIApiEndpointResult { get; set; }
         internal List<HealthCheckSetting> HealthChecks { get; set; } = new List<HealthCheckSetting>();
         internal List<WebHookNotification> Webhooks { get; set; } = new List<WebHookNotification>();
         internal bool DisableMigrations { get; set; } = false;

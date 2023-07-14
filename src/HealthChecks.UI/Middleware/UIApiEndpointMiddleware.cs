45c11cb781da0321c0187c236f64e41eed8236c0
Ivan Maximov <sungam3r@yandex.ru>
2023-07-14 11:08:21 +0300

    more STJ

--- src/HealthChecks.UI/Middleware/UIApiEndpointMiddleware.cs
+++ src/HealthChecks.UI/Middleware/UIApiEndpointMiddleware.cs
@@ -1,70 +1,68 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using HealthChecks.UI.Configuration;
 using HealthChecks.UI.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
 
-namespace HealthChecks.UI.Middleware
+namespace HealthChecks.UI.Middleware;
+
+internal class UIApiEndpointMiddleware
 {
-    internal class UIApiEndpointMiddleware
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly Settings _settings;
+
+    public UIApiEndpointMiddleware(RequestDelegate next, IServiceScopeFactory serviceScopeFactory, IOptions<Settings> settings)
     {
-        private readonly JsonSerializerSettings _jsonSerializationSettings;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
-        private readonly Settings _settings;
-
-        public UIApiEndpointMiddleware(RequestDelegate next, IServiceScopeFactory serviceScopeFactory, IOptions<Settings> settings)
+        _ = next;
+        _serviceScopeFactory = serviceScopeFactory;
+        _settings = Guard.ThrowIfNull(settings?.Value);
+        _jsonSerializerOptions = new JsonSerializerOptions
         {
-            _ = next;
-            _serviceScopeFactory = serviceScopeFactory;
-            _settings = Guard.ThrowIfNull(settings?.Value);
-            _jsonSerializationSettings = new JsonSerializerSettings
+            PropertyNameCaseInsensitive = true,
+            Converters =
             {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                Converters = new[] { new StringEnumConverter() },
-                DateTimeZoneHandling = DateTimeZoneHandling.Local
-            };
-        }
+                // allowIntegerValues: true https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/issues/1422
+                new JsonStringEnumConverter(namingPolicy: null, allowIntegerValues: true)
+            }
+        };
+    }
 
-        public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        using var db = scope.ServiceProvider.GetRequiredService<HealthChecksDb>();
+
+        var healthChecks = await db.Configurations.ToListAsync().ConfigureAwait(false);
+
+        var healthChecksExecutions = new List<HealthCheckExecution>();
+
+        foreach (var item in healthChecks.OrderBy(h => h.Id))
         {
-            using var scope = _serviceScopeFactory.CreateScope();
-            using var db = scope.ServiceProvider.GetRequiredService<HealthChecksDb>();
-
-            var healthChecks = await db.Configurations.ToListAsync().ConfigureAwait(false);
-
-            var healthChecksExecutions = new List<HealthCheckExecution>();
-
-            foreach (var item in healthChecks.OrderBy(h => h.Id))
-            {
-                var execution = await db.Executions
-                            .Include(le => le.Entries)
-                            .Where(le => le.Name == item.Name)
-                            .AsNoTracking()
-                            .SingleOrDefaultAsync()
-                            .ConfigureAwait(false);
-
-                if (execution != null)
-                {
-                    execution.History = await db.HealthCheckExecutionHistories
-                        .Where(eh => EF.Property<int>(eh, "HealthCheckExecutionId") == execution.Id)
-                        .OrderByDescending(eh => eh.On)
-                        .Take(_settings.MaximumExecutionHistoriesPerEndpoint)
-                        .ToListAsync()
+            var execution = await db.Executions
+                        .Include(le => le.Entries)
+                        .Where(le => le.Name == item.Name)
+                        .AsNoTracking()
+                        .SingleOrDefaultAsync()
                         .ConfigureAwait(false);
 
-                    healthChecksExecutions.Add(execution);
-                }
+            if (execution != null)
+            {
+                execution.History = await db.HealthCheckExecutionHistories
+                    .Where(eh => EF.Property<int>(eh, "HealthCheckExecutionId") == execution.Id)
+                    .OrderByDescending(eh => eh.On)
+                    .Take(_settings.MaximumExecutionHistoriesPerEndpoint)
+                    .ToListAsync()
+                    .ConfigureAwait(false);
+
+                healthChecksExecutions.Add(execution);
             }
-
-            _settings.ConfigureUIApiEndpointResult?.Invoke(healthChecksExecutions);
-            var responseContent = JsonConvert.SerializeObject(healthChecksExecutions, _jsonSerializationSettings);
-            context.Response.ContentType = Keys.DEFAULT_RESPONSE_CONTENT_TYPE;
-
-            await context.Response.WriteAsync(responseContent).ConfigureAwait(false);
         }
+
+        _settings.ConfigureUIApiEndpointResult?.Invoke(healthChecksExecutions);
+        await context.Response.WriteAsJsonAsync(healthChecksExecutions, _jsonSerializerOptions).ConfigureAwait(false);
     }
 }

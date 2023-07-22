1f04c7eeeacbbf662c6556db7e194616a261d73a
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2023-07-22 19:13:32 +0200

    Update

--- src/HealthChecks.Dapr/DaprHealthCheck.cs
+++ src/HealthChecks.Dapr/DaprHealthCheck.cs
@@ -1,24 +1,24 @@
 using Dapr.Client;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-namespace HealthChecks.Dapr
+namespace HealthChecks.Dapr;
+
+public class DaprHealthCheck : IHealthCheck
 {
-    public class DaprHealthCheck : IHealthCheck
+    private readonly DaprClient _daprClient;
+
+    public DaprHealthCheck(DaprClient daprClient)
     {
-        private readonly DaprClient _daprClient;
+        _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
+    }
 
-        public DaprHealthCheck(DaprClient daprClient)
-        {
-            _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
-        }
-
-        public async Task<HealthCheckResult> CheckHealthAsync(
-            HealthCheckContext context,
-            CancellationToken cancellationToken = default)
-        {
-            return await _daprClient.CheckHealthAsync(cancellationToken).ConfigureAwait(false)
-                ? new HealthCheckResult(HealthStatus.Healthy)
-                : new HealthCheckResult(context.Registration.FailureStatus);
-        }
+    /// <inheritdoc />
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        return await _daprClient.CheckHealthAsync(cancellationToken).ConfigureAwait(false)
+            ? new HealthCheckResult(HealthStatus.Healthy)
+            : new HealthCheckResult(context.Registration.FailureStatus);
     }
 }
--- src/HealthChecks.Dapr/DependencyInjection/DaprHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Dapr/DependencyInjection/DaprHealthCheckBuilderExtensions.cs
@@ -2,66 +2,68 @@ using Dapr.Client;
 using HealthChecks.Dapr;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods to configure <see cref="DaprHealthCheck"/>.
+/// </summary>
+public static class DaprHealthCheckBuilderExtensions
 {
-    public static class DaprHealthCheckBuilderExtensions
+    private const string DAPR_NAME = "dapr";
+
+    /// <summary>
+    /// Add a health check for Dapr using provided <see cref="DaprClient"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="daprClient">The <see cref="DaprClient"/> to be used.</param>
+    /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'dapr' will be used for the name.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+    /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+    /// <param name="timeout">An System.TimeSpan representing the timeout of the check. Optional.</param>
+    /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
+    public static IHealthChecksBuilder AddDapr(
+        this IHealthChecksBuilder builder,
+        DaprClient daprClient,
+        string? name = default,
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default)
     {
-        private const string DAPR_NAME = "dapr";
+        return builder.Add(new HealthCheckRegistration(
+            name ?? DAPR_NAME,
+            sp => new DaprHealthCheck(daprClient),
+            failureStatus,
+            tags,
+            timeout));
+    }
 
-        /// <summary>
-        /// Add a health check for Dapr using provided <see cref="DaprClient"/>.
-        /// </summary>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="daprClient">The DaprClient to be used. <see cref="DaprClient"/></param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'dapr' will be used for the name.</param>
-        /// <param name="failureStatus">
-        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
-        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
-        /// </param>
-        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
-        /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
-        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
-        public static IHealthChecksBuilder AddDapr(
-            this IHealthChecksBuilder builder,
-            DaprClient daprClient,
-            string? name = default,
-            HealthStatus? failureStatus = default,
-            IEnumerable<string>? tags = default,
-            TimeSpan? timeout = default)
-        {
-            return builder.Add(new HealthCheckRegistration(
-                name ?? DAPR_NAME,
-                sp => new DaprHealthCheck(daprClient),
-                failureStatus,
-                tags,
-                timeout));
-        }
-
-        /// <summary>
-        /// Add a health check for Dapr using <see cref="DaprClient"/> from service provider 
-        /// </summary>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'dapr' will be used for the name.</param>
-        /// <param name="failureStatus">
-        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
-        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
-        /// </param>
-        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
-        /// <param name="timeout">An optional System.TimeSpan representing the timeout of the check.</param>
-        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
-        public static IHealthChecksBuilder AddDapr(
-            this IHealthChecksBuilder builder,
-            string? name = default,
-            HealthStatus? failureStatus = default,
-            IEnumerable<string>? tags = default,
-            TimeSpan? timeout = default)
-        {
-            return builder.Add(new HealthCheckRegistration(
-                name ?? DAPR_NAME,
-                sp => new DaprHealthCheck(sp.GetRequiredService<DaprClient>()),
-                failureStatus,
-                tags,
-                timeout));
-        }
+    /// <summary>
+    /// Add a health check for Dapr using <see cref="DaprClient"/> from service provider.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'dapr' will be used for the name.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+    /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+    /// <param name="timeout">An System.TimeSpan representing the timeout of the check. Optoinal.</param>
+    /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
+    public static IHealthChecksBuilder AddDapr(
+        this IHealthChecksBuilder builder,
+        string? name = default,
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default)
+    {
+        return builder.Add(new HealthCheckRegistration(
+            name ?? DAPR_NAME,
+            sp => new DaprHealthCheck(sp.GetRequiredService<DaprClient>()),
+            failureStatus,
+            tags,
+            timeout));
     }
 }

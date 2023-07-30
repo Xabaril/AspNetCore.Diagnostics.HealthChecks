23c7dcdad9a81e15c4b72ab0a417c3339220d492
Ivan Maximov <sungam3r@yandex.ru>
2023-07-30 20:51:25 +0300

    Cleanup (#1966)

--- test/HealthChecks.Publisher.Datadog.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.Publisher.Datadog.Tests/DependencyInjection/RegistrationTests.cs
@@ -5,10 +5,10 @@ public class datadog_publisher_registration_should
     [Fact]
     public void add_healthcheck_when_properly_configured()
     {
-        var services = new ServiceCollection();
-        services
+        var services = new ServiceCollection()
             .AddHealthChecks()
-            .AddDatadogPublisher(serviceCheckName: "serviceCheckName", datadogAgentName: "127.0.0.1");
+            .AddDatadogPublisher(serviceCheckName: "serviceCheckName", datadogAgentName: "127.0.0.1")
+            .Services;
 
         using var serviceProvider = services.BuildServiceProvider();
         var publisher = serviceProvider.GetService<IHealthCheckPublisher>();

0f6818a9730e7981b19b3bc0efa3552d9a8125d5
Mateusz <mateusz.cieciura@gmail.com>
2023-08-07 09:10:50 +0200

    DocumentDb - Add option to check database and collection (#1985)

--- src/HealthChecks.DocumentDb/DocumentDbOptions.cs
+++ src/HealthChecks.DocumentDb/DocumentDbOptions.cs
@@ -8,4 +8,8 @@ public class DocumentDbOptions
     public string UriEndpoint { get; set; } = null!;
 
     public string PrimaryKey { get; set; } = null!;
+
+    public string? DatabaseName { get; set; }
+
+    public string? CollectionName { get; set; }
 }

21d051e7dc3b7cc719c4f634e94a74361c5bbe8f
Valentin Marin <valentin.marin@additiv.com>
2023-08-15 23:00:09 +0300

    Remove StringComparison.OrdinalIgnoreCase as EF Core translation (#1995)

--- src/HealthChecks.UI/Core/HostedService/HealthCheckReportCollector.cs
+++ src/HealthChecks.UI/Core/HostedService/HealthCheckReportCollector.cs
@@ -198,10 +198,12 @@ internal sealed class HealthCheckReportCollector : IHealthCheckReportCollector, 
 
     private async Task<bool> ShouldNotifyAsync(string healthCheckName)
     {
+#pragma warning disable RCS1155 // Use StringComparison when comparing strings, see https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/pull/1995
         var lastNotifications = await _db.Failures
-           .Where(lf => string.Equals(lf.HealthCheckName, healthCheckName, StringComparison.OrdinalIgnoreCase))
+           .Where(lf => string.Equals(lf.HealthCheckName, healthCheckName))
            .OrderByDescending(lf => lf.LastNotified)
            .Take(2).ToListAsync();
+#pragma warning restore RCS1155 // Use StringComparison when comparing strings.
 
         if (lastNotifications?.Count == 2)
         {

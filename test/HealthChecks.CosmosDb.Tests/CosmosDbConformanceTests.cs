746b079091b1a85318f951229303dc1a1c1da300
Adam Sitnik <adam.sitnik@gmail.com>
2023-09-18 22:11:28 +0200

    test fixes

--- test/HealthChecks.CosmosDb.Tests/CosmosDbConformanceTests.cs
+++ test/HealthChecks.CosmosDb.Tests/CosmosDbConformanceTests.cs
@@ -1,24 +1,17 @@
-using Azure.Identity;
 using Microsoft.Azure.Cosmos;
 
 namespace HealthChecks.CosmosDb.Tests;
 
 public class CosmosDbConformanceTests : ConformanceTests<CosmosClient, AzureCosmosDbHealthCheck, AzureCosmosDbHealthCheckOptions>
 {
+    // Sample raw connection string taken from https://github.com/Azure/azure-cosmos-dotnet-v3/blob/258d960ae3caa3ad989f60d5e656544c35006d0a/Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosClientTests.cs#L34
+    private const string ConnectionString = "AccountEndpoint=https://localtestcosmos.documents.azure.com:443/;AccountKey=425Mcv8CXQqzRNCgFNjIhT424GK99CKJvASowTnq15Vt8LeahXTcN5wt3342vQ==;";
+
     protected override IHealthChecksBuilder AddHealthCheck(IHealthChecksBuilder builder, Func<IServiceProvider, CosmosClient>? clientFactory = null, Func<IServiceProvider, AzureCosmosDbHealthCheckOptions>? optionsFactory = null, string? healthCheckName = null, HealthStatus? failureStatus = null, IEnumerable<string>? tags = null, TimeSpan? timeout = null)
         => builder.AddAzureCosmosDB(clientFactory, optionsFactory, healthCheckName, failureStatus, tags, timeout);
 
     protected override CosmosClient CreateClientForNonExistingEndpoint()
-        => new CosmosClient(
-            "https://www.thisisnotarealurl.com",
-            new DefaultAzureCredential(),
-            new CosmosClientOptions()
-            {
-                RequestTimeout = TimeSpan.FromMilliseconds(10),
-                MaxRetryAttemptsOnRateLimitedRequests = 0,
-                MaxRetryWaitTimeOnRateLimitedRequests = TimeSpan.Zero,
-                ApplicationRegion = Regions.EastUS2,
-            });
+        => new(ConnectionString);
 
     protected override AzureCosmosDbHealthCheck CreateHealthCheck(CosmosClient client, AzureCosmosDbHealthCheckOptions? options)
         => new(client, options);

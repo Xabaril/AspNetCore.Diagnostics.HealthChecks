5be2f5554ee7e37c854fd5659579d59c53d58fd4
Bradley Grainger <bgrainger@gmail.com>
2023-12-10 23:18:25 -0800

    Use MySqlDataSource in `AddMySql` by default (#2096)
    * Send a MySQL ping packet by default. Fixes #2031
    
    Users can opt in to setting a command that will be executed on the server, but the default is now a more efficient ping.
    
    * Add AddMySql overload for MySqlDataSource.
    
    This pushes users towards good defaults: firstly registering a MySqlDataSource as a singleton in DI, then just calling .AddHealthChecks().AddMySql() with no other arguments to retrieve and use that same data source for health checks.
    
    * Add package README for HealthChecks.MySql.
    
    * Use new MySqlHealthCheckOptions API.
    
    There are two constructors: one to initialise with a MySqlDataSource, and one with a connection string. This helps enforce correct uasge of the API by ensuring that exactly one is set.

--- src/HealthChecks.MySql/MySqlHealthCheck.cs
+++ src/HealthChecks.MySql/MySqlHealthCheck.cs
@@ -12,8 +12,11 @@ public class MySqlHealthCheck : IHealthCheck
 
     public MySqlHealthCheck(MySqlHealthCheckOptions options)
     {
-        Guard.ThrowIfNull(options.ConnectionString, true);
-        Guard.ThrowIfNull(options.CommandText, true);
+        Guard.ThrowIfNull(options);
+        if (options.DataSource is null && options.ConnectionString is null)
+            throw new InvalidOperationException("One of options.DataSource or options.ConnectionString must be specified.");
+        if (options.DataSource is not null && options.ConnectionString is not null)
+            throw new InvalidOperationException("Only one of options.DataSource or options.ConnectionString must be specified.");
         _options = options;
     }
 
@@ -22,18 +25,30 @@ public class MySqlHealthCheck : IHealthCheck
     {
         try
         {
-            using var connection = new MySqlConnection(_options.ConnectionString);
+            using var connection = _options.DataSource is not null ?
+                _options.DataSource.CreateConnection() :
+                new MySqlConnection(_options.ConnectionString);
 
             _options.Configure?.Invoke(connection);
             await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
 
-            using var command = connection.CreateCommand();
-            command.CommandText = _options.CommandText;
-            object? result = await command.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false);
+            if (_options.CommandText is { } commandText)
+            {
+                using var command = connection.CreateCommand();
+                command.CommandText = _options.CommandText;
+                object? result = await command.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false);
 
-            return _options.HealthCheckResultBuilder == null
-                ? HealthCheckResult.Healthy()
-                : _options.HealthCheckResultBuilder(result);
+                return _options.HealthCheckResultBuilder == null
+                    ? HealthCheckResult.Healthy()
+                    : _options.HealthCheckResultBuilder(result);
+            }
+            else
+            {
+                var success = await connection.PingAsync(cancellationToken).ConfigureAwait(false);
+                return _options.HealthCheckResultBuilder is null
+                    ? (success ? HealthCheckResult.Healthy() : new HealthCheckResult(context.Registration.FailureStatus)) :
+                    _options.HealthCheckResultBuilder(success);
+            }
         }
         catch (Exception ex)
         {
--- src/HealthChecks.MySql/MySqlHealthCheckOptions.cs
+++ src/HealthChecks.MySql/MySqlHealthCheckOptions.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using MySqlConnector;
 
@@ -10,14 +9,53 @@ namespace HealthChecks.MySql;
 public class MySqlHealthCheckOptions
 {
     /// <summary>
-    /// The MySQL connection string to be used.
+    /// Creates an instance of <see cref="MySqlHealthCheckOptions"/>.
     /// </summary>
-    public string ConnectionString { get; set; } = null!;
+    /// <param name="dataSource">The <see cref="MySqlDataSource" /> to be used.</param>
+    /// <remarks>
+    /// Depending on how the <see cref="MySqlDataSource" /> was configured, the connections it hands out may be pooled.
+    /// That is why it should be the exact same <see cref="MySqlDataSource" /> that is used by other parts of your app.
+    /// </remarks>
+    public MySqlHealthCheckOptions(MySqlDataSource dataSource)
+    {
+        DataSource = Guard.ThrowIfNull(dataSource);
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref="MySqlHealthCheckOptions"/>.
+    /// </summary>
+    /// <param name="connectionString">The MySQL connection string to be used.</param>
+    /// <remarks>
+    /// <see cref="MySqlDataSource"/> supports additional configuration beyond the connection string, such as logging and naming pools for diagnostics.
+    /// To specify a data source, use <see cref=" MySqlDataSourceBuilder"/> and the <see cref="MySqlHealthCheckOptions(MySqlDataSource)"/> constructor.
+    /// </remarks>
+    public MySqlHealthCheckOptions(string connectionString)
+    {
+        ConnectionString = Guard.ThrowIfNull(connectionString, throwOnEmptyString: true);
+    }
+
+    /// <summary>
+    /// The MySQL data source to be used.
+    /// </summary>
+    /// <remarks>
+    /// Depending on how the <see cref="MySqlDataSource" /> was configured, the connections it hands out may be pooled.
+    /// That is why it should be the exact same <see cref="MySqlDataSource" /> that is used by other parts of your app.
+    /// </remarks>
+    public MySqlDataSource? DataSource { get; }
+
+    /// <summary>
+    /// The MySQL connection string to be used, if <see cref="DataSource"/> isn't set.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="MySqlDataSource"/> supports additional configuration beyond the connection string, such as logging and naming pools for diagnostics.
+    /// To specify a data source, use <see cref=" MySqlDataSourceBuilder"/> and the <see cref="MySqlHealthCheckOptions(MySqlDataSource)"/> constructor.
+    /// </remarks>
+    public string? ConnectionString { get; }
 
     /// <summary>
     /// The query to be executed.
     /// </summary>
-    public string CommandText { get; set; } = MySqlHealthCheckBuilderExtensions.HEALTH_QUERY;
+    public string? CommandText { get; set; }
 
     /// <summary>
     /// An optional action executed before the connection is opened in the health check.

73abc7a094f9ef625b83455898a21d22d2fa6a82
Ailton Pinto <ailtonguitar@gmail.com>
2024-01-24 16:11:32 -0300

    feat: add mongodb configuration overloads (#2121)

--- src/HealthChecks.MongoDb/MongoDbHealthCheck.cs
+++ src/HealthChecks.MongoDb/MongoDbHealthCheck.cs
@@ -8,7 +8,7 @@ namespace HealthChecks.MongoDb;
 public class MongoDbHealthCheck : IHealthCheck
 {
     private static readonly BsonDocumentCommand<BsonDocument> _command = new(BsonDocument.Parse("{ping:1}"));
-    private static readonly ConcurrentDictionary<string, MongoClient> _mongoClient = new();
+    private static readonly ConcurrentDictionary<string, IMongoClient> _mongoClient = new();
     private readonly MongoClientSettings _mongoClientSettings;
     private readonly string? _specifiedDatabase;
 
@@ -19,6 +19,12 @@ public class MongoDbHealthCheck : IHealthCheck
         {
             _specifiedDatabase = MongoUrl.Create(connectionString)?.DatabaseName;
         }
+    }
+
+    public MongoDbHealthCheck(IMongoClient client, string? databaseName = default)
+        : this(client.Settings, databaseName)
+    {
+        _mongoClient[_mongoClientSettings.ToString()] = client;
     }
 
     public MongoDbHealthCheck(MongoClientSettings clientSettings, string? databaseName = default)

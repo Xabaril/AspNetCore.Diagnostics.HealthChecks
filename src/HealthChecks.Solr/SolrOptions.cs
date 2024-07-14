c29402e0a3a8dd43cc9da07582a82b7181b11e03
Hal Blakeslee <hal@hyprsoft.com>
2024-07-14 16:09:11 -0700

    2261 Add basic authentication support to Solr health check

--- src/HealthChecks.Solr/SolrOptions.cs
+++ src/HealthChecks.Solr/SolrOptions.cs
@@ -6,12 +6,18 @@ public class SolrOptions
 
     public string Core { get; private set; } = null!;
 
+    public string? Username { get; private set; }
+
+    public string? Password { get; private set; }
+
     public TimeSpan Timeout { get; private set; }
 
-    public SolrOptions UseServer(string uri, string core, TimeSpan? timeout)
+    public SolrOptions UseServer(string uri, string core, string? username, string? password, TimeSpan? timeout)
     {
         Uri = Guard.ThrowIfNull(uri);
         Core = Guard.ThrowIfNull(core);
+        Username = username;
+        Password = password;
         Timeout = timeout ?? TimeSpan.FromMilliseconds(1000);
 
         return this;

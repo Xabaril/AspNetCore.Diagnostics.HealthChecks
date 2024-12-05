8624312dd262dec8c0b9941a53d5007e6e4dbcfe
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2024-12-06 01:08:17 +0330

    Add an overload to Kafka extentions for registraing multiple instances

--- src/HealthChecks.Kafka/DependencyInjection/KafkaHealthCheckBuilderExtensions.cs
+++ src/HealthChecks.Kafka/DependencyInjection/KafkaHealthCheckBuilderExtensions.cs
@@ -113,4 +113,36 @@ public static class KafkaHealthCheckBuilderExtensions
             tags,
             timeout));
     }
+
+    /// <summary>
+    /// Add a health check for Kafka cluster.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="optionFactory">Options to configure Kafka health check.</param>
+    /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'kafka' will be used for the name.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+    /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddKafka(
+        this IHealthChecksBuilder builder,
+       Func<IServiceProvider, KafkaHealthCheckOptions> optionFactory,
+        string? name = default,
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default)
+    {
+        Guard.ThrowIfNull(builder);
+        Guard.ThrowIfNull(optionFactory);
+
+        return builder.Add(new HealthCheckRegistration(
+            name ?? NAME,
+            sp => new KafkaHealthCheck(optionFactory.Invoke(sp)),
+            failureStatus,
+            tags,
+            timeout));
+    }
 }

40fd47b2403ecce35c9fb7efba25fa0e8bbbded9
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2024-12-05 19:58:48 +0330

    Use recommnded NATS.Net client for nats package

--- test/HealthChecks.Nats.Tests/DependencyInjection/RegistrationTests.cs
+++ test/HealthChecks.Nats.Tests/DependencyInjection/RegistrationTests.cs
@@ -1,3 +1,4 @@
+using NATS.Client.Core;
 using static HealthChecks.Nats.Tests.Defines;
 
 namespace HealthChecks.Nats.Tests.DependencyInjection;
@@ -5,67 +6,45 @@ namespace HealthChecks.Nats.Tests.DependencyInjection;
 public class nats_registration_should
 {
     [Fact]
-    public void add_health_check_when_properly_configured_locally() =>
-        RegistrationFact(
-            _ => _.Url = DefaultLocalConnectionString,
-            check => check.ShouldBeOfType<NatsHealthCheck>());
-
-    [Fact]
-    public void add_named_health_check_when_properly_configured_multiple_local_instances() =>
-        RegistrationFact(
-            _ => _.Url = MixedLocalUrl,
-            check => check.ShouldBeOfType<NatsHealthCheck>(),
-            name: CustomRegistrationName);
-
-    [Fact]
-    public void add_health_check_when_demo_instance_properly_configured() =>
-        RegistrationFact(
-            setup => setup.Url = DemoConnectionString,
-            check => check.ShouldBeOfType<NatsHealthCheck>());
-
-    [Fact]
-    public void add_health_check_with_service_provider()
+    public void add_health_check_when_properly_configured()
     {
         var services = new ServiceCollection();
-
-        services
-            .AddSingleton<IDependency, Dependency>()
-            .AddHealthChecks()
-            .AddNats((sp, setup) => setup.Url = sp.GetRequiredService<IDependency>().ConnectionString);
+        services.AddHealthChecks()
+            .AddNats(ClientFactory);
 
         using var serviceProvider = services.BuildServiceProvider();
         var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
 
         var registration = options.Value.Registrations.First();
-        registration.Name.ShouldBe(NatsName);
-
         var check = registration.Factory(serviceProvider);
 
-        check.ShouldBeOfType<NatsHealthCheck>();
+        registration.Name.ShouldBe("nats");
+        check.ShouldBeOfType<NatsConnection>();
     }
 
-    private void RegistrationFact(Action<NatsOptions> setup, Action<IHealthCheck> assert, string? name = null)
+    [Fact]
+    public void add_named_health_check_when_properly_configured()
     {
         var services = new ServiceCollection();
-        services.AddHealthChecks().AddNats(setup, name);
+        services.AddHealthChecks()
+            .AddNats(clientFactory: ClientFactory, name: "nats");
 
-        var serviceProvider = services.BuildServiceProvider();
+        using var serviceProvider = services.BuildServiceProvider();
         var options = serviceProvider.GetRequiredService<IOptions<HealthCheckServiceOptions>>();
 
         var registration = options.Value.Registrations.First();
-        registration.Name.ShouldBe(name ?? NatsName);
-
         var check = registration.Factory(serviceProvider);
-        assert(check);
+
+        registration.Name.ShouldBe("nats");
+        check.ShouldBeOfType<NatsConnection>();
     }
 
-    private interface IDependency
+    private NatsConnection ClientFactory(IServiceProvider _)
     {
-        string ConnectionString { get; }
-    }
-
-    private class Dependency : IDependency
-    {
-        public string ConnectionString => DemoConnectionString;
+        var options = NatsOpts.Default with
+        {
+            Url = DefaultLocalConnectionString,
+        };
+        return new NatsConnection(options);
     }
 }
--- test/HealthChecks.Nats.Tests/Functional/NatsHealthCheckTests.cs
+++ test/HealthChecks.Nats.Tests/Functional/NatsHealthCheckTests.cs
@@ -1,8 +1,5 @@
 using System.Net;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using HealthChecks.UI.Client;
-using HealthChecks.UI.Core;
+using NATS.Client.Core;
 using static HealthChecks.Nats.Tests.Defines;
 
 namespace HealthChecks.Nats.Tests.Functional;
@@ -10,116 +7,99 @@ namespace HealthChecks.Nats.Tests.Functional;
 public class nats_healthcheck_should
 {
     [Fact]
-    public Task be_healthy_if_nats_is_available_locally() =>
-        FactAsync(
-            setup => setup.Url = DefaultLocalConnectionString,
-            async response => response.StatusCode.ShouldBe(HttpStatusCode.OK, await response.Content.ReadAsStringAsync()));
-
-    [Fact]
-    public Task be_healthy_for_official_demo_instance() =>
-        FactAsync(
-            setup => setup.Url = DemoConnectionString,
-            async response => response.StatusCode.ShouldBe(HttpStatusCode.OK, await response.Content.ReadAsStringAsync()));
-
-    [Fact]
-    public Task be_healthy_if_nats_is_available_and_has_custom_name() =>
-        FactAsync(
-            setup => setup.Url = DefaultLocalConnectionString,
-            async response => response.StatusCode.ShouldBe(HttpStatusCode.OK, await response.Content.ReadAsStringAsync()),
-            name: "Demo");
-
-    [Fact]
-    public Task be_unhealthy_if_nats_endpoint_does_not_exist_or_is_offline() =>
-        FactAsync(
-            setup => setup.Url = ConnectionStringDoesNotExistOrStopped,
-            async response =>
-            {
-                response.StatusCode.ShouldBe(HttpStatusCode.ServiceUnavailable);
-                var content = await response.Content.ReadAsStringAsync();
-                var report = JsonSerializer.Deserialize<UIHealthReport>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() } })!;
-                report.Status.ShouldBe(UIHealthStatus.Unhealthy);
-                report.Entries["nats"].Exception.ShouldBe("Failed to connect");
-
-            });
-
-    [Fact]
-    public Task be_unhealthy_if_nats_endpoint_is_bogus() =>
-        FactAsync(
-            setup => setup.Url = "bogus",
-            async response =>
-            {
-                response.StatusCode.ShouldBe(HttpStatusCode.ServiceUnavailable);
-                var content = await response.Content.ReadAsStringAsync();
-                var report = JsonSerializer.Deserialize<UIHealthReport>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() } })!;
-                report.Status.ShouldBe(UIHealthStatus.Unhealthy);
-                report.Entries["nats"].Exception.ShouldBe("Failed to connect");
-            });
-
-    [Fact]
-    public Task be_unhealthy_if_nats_is_available_but_credentials_path_doesnt_exist() =>
-        FactAsync(
-            setup =>
-            {
-                setup.Url = DefaultLocalConnectionString;
-                setup.CredentialsPath = CredentialsPathDoesnExist;
-            },
-            response => response.StatusCode.ShouldBe(HttpStatusCode.ServiceUnavailable),
-            name: CredentialsPathDoesnExist);
-
-    [Fact]
-    public Task be_unhealthy_if_nats_20_credentials_missing() =>
-        FactAsync(
-            setup =>
-            {
-                setup.Url = DefaultLocalConnectionString;
-                setup.CredentialsPath = string.Empty;
-                setup.Jwt = "jwt";
-                setup.PrivateNKey = CredentialsPathDoesnExist;
-            },
-            response => response.StatusCode.ShouldBe(HttpStatusCode.ServiceUnavailable),
-            name: CredentialsPathDoesnExist);
-
-    private async Task FactAsync(Action<NatsOptions> setupAction, Action<HttpResponseMessage> assertAction, string? name = null)
+    public async Task be_healthy_when_nats_is_available_using_client_factory()
     {
         var webHostBuilder = new WebHostBuilder()
-            .ConfigureServices(services => services
+            .ConfigureServices(services =>
+            {
+                services
                     .AddHealthChecks()
                     .AddNats(
-                        setupAction,
-                        name: name,
-                        tags: Tags))
-            .Configure(ConfigureApplicationBuilder);
+                        clientFactory: sp =>
+                        {
+                            var options = NatsOpts.Default with
+                            {
+                                Url = DefaultLocalConnectionString,
+                            };
+                            return new NatsConnection(options);
+                        }, tags: new string[] { "nats" });
+            })
+            .Configure(app =>
+            {
+                app.UseHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = r => r.Tags.Contains("nats")
+                });
+            });
 
         using var server = new TestServer(webHostBuilder);
 
-        var response = await server.CreateRequest(HealthRequestRelativePath).GetAsync().ConfigureAwait(false);
+        using var response = await server.CreateRequest("/health").GetAsync();
 
-        assertAction(response);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
     }
 
-    private async Task FactAsync(Action<NatsOptions> setupAction, Func<HttpResponseMessage, Task> asyncAssertAction, string? name = null)
+    [Fact]
+    public async Task be_healthy_when_nats_is_available_using_singleton()
     {
         var webHostBuilder = new WebHostBuilder()
-            .ConfigureServices(services => services
+            .ConfigureServices(services =>
+            {
+                var options = NatsOpts.Default with
+                {
+                    Url = DefaultLocalConnectionString,
+                };
+
+                services
+                    .AddSingleton<INatsConnection>(new NatsConnection(options))
                     .AddHealthChecks()
-                    .AddNats(
-                        setupAction,
-                        name: name,
-                        tags: Tags))
-            .Configure(ConfigureApplicationBuilder);
+                    .AddNats(tags: new string[] { "nats" });
+            })
+            .Configure(app =>
+            {
+                app.UseHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = r => r.Tags.Contains("nats")
+                });
+            });
 
         using var server = new TestServer(webHostBuilder);
 
-        var response = await server.CreateRequest(HealthRequestRelativePath).GetAsync().ConfigureAwait(false);
+        using var response = await server.CreateRequest("/health").GetAsync();
 
-        await asyncAssertAction(response).ConfigureAwait(false);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
     }
 
-    private void ConfigureApplicationBuilder(IApplicationBuilder app) =>
-        app.UseHealthChecks(HealthRequestRelativePath, new HealthCheckOptions
-        {
-            Predicate = r => r.Tags.Contains(NatsName) || r.Name == NatsName,
-            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
-            AllowCachingResponses = false
-        });
+    [Fact]
+    public async Task be_unhealthy_when_nats_is_unavailable()
+    {
+        var webHostBuilder = new WebHostBuilder()
+            .ConfigureServices(services =>
+            {
+                services
+                    .AddHealthChecks()
+                    .AddNats(
+                        clientFactory: sp =>
+                        {
+                            var options = NatsOpts.Default with
+                            {
+                                Url = ConnectionStringDoesNotExistOrStopped,
+                            };
+                            return new NatsConnection(options);
+                        }, tags: new string[] { "nats" });
+            })
+            .Configure(app =>
+            {
+                app.UseHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = r => r.Tags.Contains("nats")
+                });
+            });
+
+        using var server = new TestServer(webHostBuilder);
+
+        using var response = await server.CreateRequest("/health").GetAsync();
+
+        response.StatusCode.ShouldBe(HttpStatusCode.ServiceUnavailable);
+    }
 }
